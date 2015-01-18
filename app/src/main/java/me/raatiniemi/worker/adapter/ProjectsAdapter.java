ce8e0c424f70777e4058ff57fabe105799fd60da
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-18 16:16:22 +0100

    Added adapter for projects.

---
+++ app/src/main/java/me/raatiniemi/worker/adapter/ProjectsAdapter.java
@@ -0,0 +1,62 @@
+package me.raatiniemi.worker.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import me.raatiniemi.worker.R;
+import me.raatiniemi.worker.data.Project;
+
+public class ProjectsAdapter extends ArrayAdapter<Project>
+{
+    private static class ViewHolder
+    {
+        TextView name;
+        TextView time;
+        TextView description;
+    }
+
+    public ProjectsAdapter(Context context, ArrayList<Project> projects)
+    {
+        super(context, R.layout.projects_item, projects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        // Get the project item for the row.
+        Project project = getItem(position);
+
+        // Check if an existing view is being reused,
+        // otherwise inflate the view.
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.projects_item, parent, false);
+
+            // Initialize the view holder and retrieve the view components.
+            viewHolder = new ViewHolder();
+            viewHolder.name = (TextView) convertView.findViewById(R.id.project_name);
+            viewHolder.time = (TextView) convertView.findViewById(R.id.project_time);
+            viewHolder.description = (TextView) convertView.findViewById(R.id.project_description);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Populate the data into the template view.
+        viewHolder.name.setText(project.getName());
+
+        // TODO: Set the actual values.
+        viewHolder.time.setText(null);
+        viewHolder.description.setText(null);
+
+        return convertView;
+    }
+}
