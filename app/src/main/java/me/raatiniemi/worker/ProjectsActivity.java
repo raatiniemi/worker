6db543803c52fcd8616458e5bb6073c4083e5c42
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-18 13:50:08 +0100

    Added activity for listing projects.

---
+++ app/src/main/java/me/raatiniemi/worker/ProjectsActivity.java
@@ -0,0 +1,14 @@
+package me.raatiniemi.worker;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ProjectsActivity extends ActionBarActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projects);
+    }
+}
