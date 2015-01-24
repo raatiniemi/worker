d051d92d8fb4d2566a4a47b2339f4f22128afe40
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-24 13:48:15 +0100

    Added the TimeMapper-class.

---
+++ app/src/main/java/me/raatiniemi/worker/mapper/TimeMapper.java
@@ -0,0 +1,25 @@
+package me.raatiniemi.worker.mapper;
+
+import android.provider.BaseColumns;
+
+public class TimeMapper extends AbstractMapper
+{
+    private static final String TABLE_NAME = "time";
+
+    private interface Columns extends BaseColumns
+    {
+        public static final String PROJECT_ID = "project_id";
+
+        public static final String START = "start";
+
+        public static final String STOP = "stop";
+    }
+
+    public static final String CREATE_TABLE =
+        "CREATE TABLE " + TABLE_NAME + " ( " +
+            Columns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            Columns.PROJECT_ID + " INTEGER NOT NULL, " +
+            Columns.START + " INTEGER NOT NULL, " +
+            Columns.STOP + " INTEGER NULL " +
+        ");";
+}
