e418732347d1ef6a87f83ae9ec2e4b3316c9880b
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-24 22:10:32 +0100

    Added getters and setters to the project domain object.

--- app/src/main/java/me/raatiniemi/worker/domain/Project.java
+++ app/src/main/java/me/raatiniemi/worker/domain/Project.java
@@ -2,8 +2,33 @@ package me.raatiniemi.worker.domain;
 
 public class Project extends DomainObject
 {
-    public Project(Long id)
+    private String mName;
+
+    private String mDescription;
+
+    public Project(Long id, String name)
     {
         super(id);
+        setName(name);
+    }
+
+    public void setName(String name)
+    {
+         mName = name;
+    }
+
+    public String getName()
+    {
+        return mName;
+    }
+
+    public void setDescription(String description)
+    {
+        mDescription = description;
+    }
+
+    public String getDescription()
+    {
+        return mDescription;
     }
 }
