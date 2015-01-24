7b10b78a5b92d9279983801e9f465828f4fc27b5
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-24 06:55:23 +0100

    Added the time data type.

---
+++ app/src/main/java/me/raatiniemi/worker/data/Time.java
@@ -0,0 +1,77 @@
+package me.raatiniemi.worker.data;
+
+public class Time
+{
+    private Long mId;
+
+    private Long mProjectId;
+
+    private Long mStart;
+
+    private Long mStop;
+
+    public void setId(Long id)
+    {
+        mId = id;
+    }
+
+    public void setId(long id)
+    {
+        setId(Long.valueOf(id));
+    }
+
+    public Long getId()
+    {
+        return mId;
+    }
+
+    public void setProjectId(Long projectId)
+    {
+        mProjectId = projectId;
+    }
+
+    public void setProjectId(long projectId)
+    {
+        setProjectId(Long.valueOf(projectId));
+    }
+
+    public Long getProjectId()
+    {
+        return mProjectId;
+    }
+
+    public void setStart(Long start)
+    {
+        mStart = start;
+    }
+
+    public void setStart(long start)
+    {
+        setStart(Long.valueOf(start));
+    }
+
+    public Long getStart()
+    {
+        return mStart;
+    }
+
+    public void setStop(Long stop)
+    {
+        mStop = stop;
+    }
+
+    public void setStop(long stop)
+    {
+        setStop(Long.valueOf(stop));
+    }
+
+    public Long getStop()
+    {
+        return mStop;
+    }
+
+    public boolean isActive()
+    {
+        return getStop() == null;
+    }
+}
