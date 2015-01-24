4c475dc7b585e95b2049d539dc816a6c72e5a6cd
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-24 07:58:04 +0100

    Added ability to start timer for project.

--- app/src/main/java/me/raatiniemi/worker/database/TimeDataSource.java
+++ app/src/main/java/me/raatiniemi/worker/database/TimeDataSource.java
@@ -1,11 +1,14 @@
 package me.raatiniemi.worker.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import me.raatiniemi.worker.data.Time;
 
@@ -76,5 +79,43 @@ public class TimeDataSource
         }
 
         return time;
+    }
+
+    public Time findTimeById(long id)
+    {
+        String[] columns = new String[]{
+            Structure.COLUMN_ID,
+            Structure.COLUMN_PROJECT_ID,
+            Structure.COLUMN_TIME_START,
+            Structure.COLUMN_TIME_STOP
+        };
+
+        String selection = Structure.COLUMN_ID +"="+ id;
+
+        Cursor row = mDatabase.query(Structure.TABLE_NAME, columns, selection, null, null, null, null);
+        if (!row.moveToFirst()) {
+            Log.d("findTimeById", "No time item with id: "+ id);
+            return null;
+        }
+
+        Time time = new Time();
+        time.setId(row.getLong(row.getColumnIndex(Structure.COLUMN_ID)));
+        time.setProjectId(row.getLong(row.getColumnIndex(Structure.COLUMN_PROJECT_ID)));
+        time.setStart(row.getLong(row.getColumnIndex(Structure.COLUMN_TIME_START)));
+        time.setStop(row.getLong(row.getColumnIndex(Structure.COLUMN_TIME_STOP)));
+
+        return time;
+    }
+
+    public Time startTimerForProject(long project_id)
+    {
+        // TODO: Check if timer is already active for project, throw exception.
+
+        ContentValues values = new ContentValues();
+        values.put(Structure.COLUMN_PROJECT_ID, project_id);
+        values.put(Structure.COLUMN_TIME_START, (new Date().getTime()));
+
+        long id = mDatabase.insert(Structure.TABLE_NAME, null, values);
+        return findTimeById(id);
     }
 }
