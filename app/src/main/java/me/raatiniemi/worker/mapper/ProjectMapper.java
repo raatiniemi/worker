ea86629b968e67ef46299120dc8f8f74c86c4efd
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-25 08:17:09 +0100

    Implemented the load and getProjects for the ProjectMapper.

--- app/src/main/java/me/raatiniemi/worker/mapper/ProjectMapper.java
+++ app/src/main/java/me/raatiniemi/worker/mapper/ProjectMapper.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.BaseColumns;
 
+import java.util.ArrayList;
+
 import me.raatiniemi.worker.domain.DomainObject;
+import me.raatiniemi.worker.domain.Project;
 
 public class ProjectMapper extends AbstractMapper
 {
@@ -35,7 +38,34 @@ public class ProjectMapper extends AbstractMapper
 
     protected DomainObject load(Cursor row)
     {
-        // TODO: Implement load for project mapper.
-        return null;
+        long id = row.getLong(row.getColumnIndex(BaseColumns._ID));
+        String name = row.getString(row.getColumnIndex(Columns.NAME));
+        String description = row.getString(row.getColumnIndex(Columns.DESCRIPTION));
+
+        Project project = new Project(id, name);
+        project.setDescription(description);
+
+        return project;
+    }
+
+    public ArrayList<Project> getProjects()
+    {
+        ArrayList<Project> result = new ArrayList<>();
+
+        String[] columns = new String[]{
+            BaseColumns._ID,
+            Columns.NAME,
+            Columns.DESCRIPTION
+        };
+
+        Cursor rows = mDatabase.query(TABLE_NAME, columns, null, null, null, null, null);
+        if (rows.moveToFirst()) {
+            do {
+                Project project = (Project) load(rows);
+                result.add(project);
+            } while (rows.moveToNext());
+        }
+
+        return result;
     }
 }
