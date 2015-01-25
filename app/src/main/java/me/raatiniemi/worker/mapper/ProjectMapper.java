03f2b0d3fc8e9f828555920e95245b05c874c7a4
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-25 08:47:20 +0100

    Added ability to create new projects with the ProjectMapper.

--- app/src/main/java/me/raatiniemi/worker/mapper/ProjectMapper.java
+++ app/src/main/java/me/raatiniemi/worker/mapper/ProjectMapper.java
@@ -1,5 +1,6 @@
 package me.raatiniemi.worker.mapper;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.provider.BaseColumns;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 
 import me.raatiniemi.worker.domain.DomainObject;
 import me.raatiniemi.worker.domain.Project;
+import me.raatiniemi.worker.exception.ProjectAlreadyExistsException;
 
 public class ProjectMapper extends AbstractMapper
 {
@@ -89,5 +91,19 @@ public class ProjectMapper extends AbstractMapper
         }
 
         return (Project) load(row);
+    }
+
+    public Project insert(Project project) throws ProjectAlreadyExistsException
+    {
+        if (null != find(project.getName())) {
+            throw new ProjectAlreadyExistsException();
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(Columns.NAME, project.getName());
+        values.put(Columns.DESCRIPTION, project.getDescription());
+
+        long id = mDatabase.insert(getTable(), null, values);
+        return (Project) find(id);
     }
 }
