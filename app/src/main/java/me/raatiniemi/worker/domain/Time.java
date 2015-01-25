ada21f4d69c4cb3781a38d3b10b4e07af30a9106
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-25 09:13:30 +0100

    Added getters and setters for the Time domain object.

--- app/src/main/java/me/raatiniemi/worker/domain/Time.java
+++ app/src/main/java/me/raatiniemi/worker/domain/Time.java
@@ -2,8 +2,47 @@ package me.raatiniemi.worker.domain;
 
 public class Time extends DomainObject
 {
-    public Time(Long id)
+    private long mProjectId;
+
+    private long mStart;
+
+    private long mStop;
+
+    public Time(Long id, Long projectId, Long start, Long stop)
     {
         super(id);
+        setProjectId(projectId);
+        setStart(start);
+        setStop(stop);
+    }
+
+    private void setProjectId(Long projectId)
+    {
+        mProjectId = projectId;
+    }
+
+    public Long getProjectId()
+    {
+        return mProjectId;
+    }
+
+    public void setStart(Long start)
+    {
+        mStart = start;
+    }
+
+    public Long getStart()
+    {
+        return mStart;
+    }
+
+    public void setStop(Long stop)
+    {
+        mStop = stop;
+    }
+
+    public Long getStop()
+    {
+        return mStop;
     }
 }
