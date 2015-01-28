e4db2a55b55dc5488041be146232f89ff8bee590
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-28 19:19:19 +0100

    Added unit test for the Time domain object.

---
+++ app/src/androidTest/java/me/raatiniemi/worker/domain/TimeTest.java
@@ -0,0 +1,12 @@
+package me.raatiniemi.worker.domain;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class TimeTest extends ApplicationTestCase<Application>
+{
+    public TimeTest()
+    {
+        super(Application.class);
+    }
+}
