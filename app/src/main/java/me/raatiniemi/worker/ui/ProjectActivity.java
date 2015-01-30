e535062860e2c5bfabd9434796744cf5b31f92f7
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-30 13:33:59 +0100

    Added activity for Project.

---
+++ app/src/main/java/me/raatiniemi/worker/ui/ProjectActivity.java
@@ -0,0 +1,16 @@
+package me.raatiniemi.worker.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import me.raatiniemi.worker.R;
+
+public class ProjectActivity extends ActionBarActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project);
+    }
+}
