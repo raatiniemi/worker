584f701c60e15af2745a90633620234ed83e19ad
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-01-30 15:12:44 +0100

    Initialize the ProjectTimeListAdapter.

--- app/src/main/java/me/raatiniemi/worker/ui/ProjectActivity.java
+++ app/src/main/java/me/raatiniemi/worker/ui/ProjectActivity.java
@@ -3,8 +3,14 @@ package me.raatiniemi.worker.ui;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import me.raatiniemi.worker.R;
+import me.raatiniemi.worker.adapter.ProjectTimeListAdapter;
+import me.raatiniemi.worker.domain.Project;
+import me.raatiniemi.worker.mapper.MapperRegistry;
+import me.raatiniemi.worker.mapper.ProjectMapper;
 
 public class ProjectActivity extends ActionBarActivity
 {
@@ -24,6 +30,20 @@ public class ProjectActivity extends ActionBarActivity
             return;
         }
 
-        // TODO: Retrieve the project data.
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        RecyclerView timeView = (RecyclerView) findViewById(R.id.project_time_list);
+        timeView.setLayoutManager(manager);
+
+        // Retrieve the project data from the mapper.
+        ProjectMapper projectMapper = MapperRegistry.getProjectMapper();
+        Project project = (Project) projectMapper.find(projectId);
+
+        // Set the activity title to the project name.
+        setTitle(project.getName());
+
+        ProjectTimeListAdapter adapter = new ProjectTimeListAdapter(project.getTime());
+        timeView.setAdapter(adapter);
     }
 }
