8de6c33ebd2cb2dad503cd73a75b6650c1eda051
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-02 20:27:28 +0100

    Documentation for the DomainObject.

--- app/src/main/java/me/raatiniemi/worker/domain/DomainObject.java
+++ app/src/main/java/me/raatiniemi/worker/domain/DomainObject.java
@@ -1,14 +1,28 @@
 package me.raatiniemi.worker.domain;
 
+/**
+ * Base class for the domain objects.
+ */
 public class DomainObject
 {
+    /**
+     * Id for the domain object.
+     */
     private Long mId;
 
+    /**
+     * Instantiate the domain object with id.
+     * @param id Id for the domain object.
+     */
     public DomainObject(Long id)
     {
         mId = id;
     }
 
+    /**
+     * Retrieve the id for the domain object.
+     * @return Id for the domain object.
+     */
     public Long getId()
     {
         return mId;
