917fdfb0c3d52eb5b35d149b205dc1a3faab5fa8
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-02 20:22:14 +0100

    Documentation for the database helper.

--- app/src/main/java/me/raatiniemi/worker/database/Helper.java
+++ app/src/main/java/me/raatiniemi/worker/database/Helper.java
@@ -8,14 +8,30 @@ import me.raatiniemi.worker.application.Worker;
 import me.raatiniemi.worker.mapper.ProjectMapper;
 import me.raatiniemi.worker.mapper.TimeMapper;
 
+/**
+ * Database helper for the "worker"-database.
+ */
 public class Helper extends SQLiteOpenHelper
 {
+    /**
+     * Name of the database.
+     */
     private static final String DATABASE_NAME = "worker";
 
+    /**
+     * Version of the database structure.
+     */
     private static final int DATABASE_VERSION = 1;
 
+    /**
+     * Instance for the database helper.
+     */
     private static Helper mHelper;
 
+    /**
+     * Retrieve the instance for the database helper, instantiate if necessary.
+     * @return Instance for the database helper.
+     */
     public static synchronized Helper getInstance()
     {
         if ( mHelper == null ) {
@@ -24,6 +40,10 @@ public class Helper extends SQLiteOpenHelper
         return mHelper;
     }
 
+    /**
+     * Instantiate the database helper with the application context.
+     * @param context Application context.
+     */
     public Helper(Context context)
     {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
