d7c0c6cb5e613cd4920ece8a9a22ff427805aa41
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-03 21:38:35 +0100

    Documentation within the Project domain object.

--- app/src/main/java/me/raatiniemi/worker/domain/Project.java
+++ app/src/main/java/me/raatiniemi/worker/domain/Project.java
@@ -34,21 +34,37 @@ public class Project extends DomainObject
         this(null, name);
     }
 
+    /**
+     * Set the project name.
+     * @param name Project name.
+     */
     public void setName(String name)
     {
          mName = name;
     }
 
+    /**
+     * Retrieve the project name.
+     * @return Project name.
+     */
     public String getName()
     {
         return mName;
     }
 
+    /**
+     * Set the project description.
+     * @param description Project description.
+     */
     public void setDescription(String description)
     {
         mDescription = description;
     }
 
+    /**
+     * Retrieve the project description.
+     * @return Project description.
+     */
     public String getDescription()
     {
         return mDescription;
@@ -103,6 +119,10 @@ public class Project extends DomainObject
         return String.format("%dh %dm", hours, minutes);
     }
 
+    /**
+     * Retrieve the time domain object that might be active.
+     * @return Time domain object, or null if no time have been registered.
+     */
     private Time getActiveTime()
     {
         ArrayList<Time> time = getTime();
@@ -114,8 +134,15 @@ public class Project extends DomainObject
         return time.get(time.size() - 1);
     }
 
+    /**
+     * Retrieve the time when the project was clocked in.
+     * @return Time when project was clocked in, or null if project is not active.
+     */
     public String getClockedInSince()
     {
+        // TODO: Just return the value for getStart() and parse it outside of the domain object.
+
+        // If the project is not active, there's nothing to do.
         if (!isActive()) {
             return null;
         }
@@ -133,8 +160,13 @@ public class Project extends DomainObject
         return format.format(date);
     }
 
+    /**
+     * Clock out the active project, if the project is not active nothing happens.
+     * @return The clocked out time domain object, or null if project is not active.
+     */
     public Time clockOut()
     {
+        // If the project is not active, there's nothing to do.
         if (!isActive()) {
             return null;
         }
@@ -145,6 +177,10 @@ public class Project extends DomainObject
         return time;
     }
 
+    /**
+     * Check if the project is active.
+     * @return True if the project is active, otherwise false.
+     */
     public boolean isActive()
     {
         boolean active = false;
