191fc8d4413f453be59de4b01ac0364bd3936e64
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-03 04:54:22 +0100

    Added the DatePickerFragment.

---
+++ app/src/main/java/me/raatiniemi/worker/ui/fragment/DatePickerFragment.java
@@ -0,0 +1,32 @@
+package me.raatiniemi.worker.ui.fragment;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener
+{
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day)
+    {
+        // Increment the given month by one, since it's zero based,
+        // e.g. January is 0 and December is 11.
+        month++;
+
+        // TODO: Handle the supplied date.
+    }
+}
