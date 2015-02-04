8ecbba73e1ce45f4b3e6e7bf8a72eaff5de4d071
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-04 05:05:06 +0100

    ClockOutAtFragment - Initialize the picker fragment classes.

--- app/src/main/java/me/raatiniemi/worker/ui/fragment/ClockOutAtFragment.java
+++ app/src/main/java/me/raatiniemi/worker/ui/fragment/ClockOutAtFragment.java
@@ -3,17 +3,36 @@ package me.raatiniemi.worker.ui.fragment;
 import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.app.TimePickerDialog;
+import android.os.Bundle;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 public class ClockOutAtFragment extends Fragment
     implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener
 {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // Initialize the "DatePicker"-fragment.
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.setOnDateSetListener(this);
+        datePickerFragment.show(getFragmentManager().beginTransaction(), "fragment_clock_out_date_picker");
+    }
+
     public void onDateSet(DatePicker view, int year, int month, int day)
     {
+        // TODO: Handle the input data.
+
+        // Initialize the "TimePicker"-fragment.
+        TimePickerFragment timePickerFragment = new TimePickerFragment();
+        timePickerFragment.setOnTimeSetListener(this);
+        timePickerFragment.show(getFragmentManager().beginTransaction(), "fragment_clock_out_date_picker");
     }
 
     public void onTimeSet(TimePicker view, int hour, int minute)
     {
+        // TODO: Handle the input data.
     }
 }
