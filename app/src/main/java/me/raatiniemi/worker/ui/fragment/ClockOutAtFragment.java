594e7321d19959e759c130e5a4bc0ea25f9a63cf
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-04 04:46:41 +0100

    ClockOutAtFragment - Added extension and interfaces for date/time picker dialog.

--- app/src/main/java/me/raatiniemi/worker/ui/fragment/ClockOutAtFragment.java
+++ app/src/main/java/me/raatiniemi/worker/ui/fragment/ClockOutAtFragment.java
@@ -1,5 +1,19 @@
 package me.raatiniemi.worker.ui.fragment;
 
-public class ClockOutAtFragment
+import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.app.TimePickerDialog;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+public class ClockOutAtFragment extends Fragment
+    implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener
 {
+    public void onDateSet(DatePicker view, int year, int month, int day)
+    {
+    }
+
+    public void onTimeSet(TimePicker view, int hour, int minute)
+    {
+    }
 }
