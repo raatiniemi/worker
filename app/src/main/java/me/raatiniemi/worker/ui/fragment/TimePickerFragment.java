3fbf922a2a924d5ae9275964fcd0f7fdb5b45290
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-04 20:50:27 +0100

    TimePickerFragment - Dismiss if no listener have been supplied.

--- app/src/main/java/me/raatiniemi/worker/ui/fragment/TimePickerFragment.java
+++ app/src/main/java/me/raatiniemi/worker/ui/fragment/TimePickerFragment.java
@@ -1,12 +1,18 @@
 package me.raatiniemi.worker.ui.fragment;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 
 import java.util.Calendar;
+
+import me.raatiniemi.worker.R;
 
 public class TimePickerFragment extends DialogFragment
 {
@@ -34,13 +40,40 @@ public class TimePickerFragment extends DialogFragment
     }
 
     @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+
+        // Check if we have a callback listener.
+        if (getOnTimeSetListener() == null) {
+            Log.e("onAttach", "No listener have been supplied for the TimePickerFragment");
+
+            // We're unable to use the TimePickerFragment
+            // since we do not have listener.
+            new AlertDialog.Builder(getActivity())
+                .setTitle(getString(R.string.time_picker_fragment_no_listener_title))
+                .setMessage(getString(R.string.time_picker_fragment_no_listener_description))
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing...
+                    }
+                })
+                .show();
+
+            // Dismiss the dialog since we are unable
+            // to properly handle events with it.
+            dismiss();
+        }
+    }
+
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int minute = calendar.get(Calendar.MINUTE);
 
-        // TODO: Handle if "getOnTimeSetListener" returns null.
         return new TimePickerDialog(getActivity(), getOnTimeSetListener(), hour, minute, DateFormat.is24HourFormat(getActivity()));
     }
 }
