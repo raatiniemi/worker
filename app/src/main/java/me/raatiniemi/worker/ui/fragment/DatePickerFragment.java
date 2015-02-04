e0f3d19609e897064b7268941e468232a230dace
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-04 20:45:12 +0100

    DatePickerFragment - Dismiss if no listener have been supplied.

--- app/src/main/java/me/raatiniemi/worker/ui/fragment/DatePickerFragment.java
+++ app/src/main/java/me/raatiniemi/worker/ui/fragment/DatePickerFragment.java
@@ -1,12 +1,17 @@
 package me.raatiniemi.worker.ui.fragment;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.widget.DatePicker;
+import android.util.Log;
 
 import java.util.Calendar;
+
+import me.raatiniemi.worker.R;
 
 public class DatePickerFragment extends DialogFragment
 {
@@ -34,6 +39,34 @@ public class DatePickerFragment extends DialogFragment
     }
 
     @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+
+        // Check if we have a callback listener.
+        if (getOnDateSetListener() == null) {
+            Log.e("onAttach", "No listener have been supplied for the DatePickerFragment");
+
+            // We're unable to use the DatePickerFragment
+            // since we do not have listener.
+            new AlertDialog.Builder(getActivity())
+                .setTitle(getString(R.string.date_picker_fragment_no_listener_title))
+                .setMessage(getString(R.string.date_picker_fragment_no_listener_description))
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing...
+                    }
+                })
+                .show();
+
+            // Dismiss the dialog since we are unable
+            // to properly handle events with it.
+            dismiss();
+        }
+    }
+
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         Calendar calendar = Calendar.getInstance();
@@ -41,7 +74,6 @@ public class DatePickerFragment extends DialogFragment
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        // TODO: Handle if "getOnDateSetListener" returns null.
         return new DatePickerDialog(getActivity(), getOnDateSetListener(), year, month, day);
     }
 }
