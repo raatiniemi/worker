5494e084580c130e1932113776be56e3a007604a
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-10 19:14:18 +0100

    Added constructors for domain related exception classes.

--- app/src/main/java/me/raatiniemi/worker/exception/DomainException.java
+++ app/src/main/java/me/raatiniemi/worker/exception/DomainException.java
@@ -2,4 +2,13 @@ package me.raatiniemi.worker.exception;
 
 public class DomainException extends Exception
 {
+    public DomainException(String message)
+    {
+        super(message);
+    }
+
+    public DomainException()
+    {
+        super();
+    }
 }
--- app/src/main/java/me/raatiniemi/worker/exception/domain/ClockActivityException.java
+++ app/src/main/java/me/raatiniemi/worker/exception/domain/ClockActivityException.java
@@ -4,4 +4,13 @@ import me.raatiniemi.worker.exception.DomainException;
 
 public class ClockActivityException extends DomainException
 {
+    public ClockActivityException(String message)
+    {
+        super(message);
+    }
+
+    public ClockActivityException()
+    {
+        super();
+    }
 }
