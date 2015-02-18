639db616136932efd2096379ff82a5e3a378da80
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-18 04:24:28 +0100

    Documentation with the ClockActivityAtFragment.

--- app/src/main/java/me/raatiniemi/worker/ui/fragment/ClockActivityAtFragment.java
+++ app/src/main/java/me/raatiniemi/worker/ui/fragment/ClockActivityAtFragment.java
@@ -17,6 +17,20 @@ public class ClockActivityAtFragment extends Fragment
     implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener
 {
     /**
+     * Public interface for the "OnClockActivityAtListener"
+     */
+    public interface OnClockActivityAtListener
+    {
+        /**
+         * Triggered after the date and time have been selected.
+         * @param project Project to clock in or out.
+         * @param calendar Calendar with date and time to clock in or out.
+         * @param index Row index from the adapter.
+         */
+        public void onClockActivityAt(Project project, Calendar calendar, int index);
+    }
+
+    /**
      * Name of the key for the project argument.
      */
     private static final String ARGUMENT_PROJECT = "_project";
@@ -38,12 +52,10 @@ public class ClockActivityAtFragment extends Fragment
      */
     private int mIndex;
 
+    /**
+     * Calendar object with the selected date and time.
+     */
     private Calendar mCalendar;
-
-    public interface OnClockActivityAtListener
-    {
-        public void onClockActivityAt(Project project, Calendar calendar, int index);
-    }
 
     /**
      * Create a new instance with the project and adapter row index.
@@ -64,8 +76,7 @@ public class ClockActivityAtFragment extends Fragment
         return fragment;
     }
 
-    @Override
-    public void onAttach(Activity activity)
+    @Override public void onAttach(Activity activity)
     {
         super.onAttach(activity);
 
@@ -93,6 +104,13 @@ public class ClockActivityAtFragment extends Fragment
         }
     }
 
+    /**
+     * Callback method from the date picker fragment.
+     * @param view Date picker view.
+     * @param year Selected year.
+     * @param month Selected month.
+     * @param day Selected day.
+     */
     public void onDateSet(DatePicker view, int year, int month, int day)
     {
         // Change the year, month, and day to the calendar.
@@ -104,6 +122,12 @@ public class ClockActivityAtFragment extends Fragment
         timePickerFragment.show(getFragmentManager().beginTransaction(), "fragment_clock_activity_time_picker");
     }
 
+    /**
+     * Callback method from the time picker fragment.
+     * @param view Time picker view.
+     * @param hour Selected hour.
+     * @param minute Selected minute.
+     */
     public void onTimeSet(TimePicker view, int hour, int minute)
     {
         mCalendar.set(Calendar.HOUR_OF_DAY, hour);
