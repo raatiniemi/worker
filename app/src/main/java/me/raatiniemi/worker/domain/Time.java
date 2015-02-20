7fcc88838455ecf2d25cae90adf3dff3164441fe
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-20 04:57:23 +0100

    Added documentation for the Time domain object.

--- app/src/main/java/me/raatiniemi/worker/domain/Time.java
+++ app/src/main/java/me/raatiniemi/worker/domain/Time.java
@@ -29,28 +29,54 @@ public class Time extends DomainObject
         this(null, projectId, (new Date()).getTime(), (long) 0);
     }
 
+    /**
+     * Set the project id to which the time is connected to.
+     * @param projectId Id for connected project.
+     */
     private void setProjectId(long projectId)
     {
         mProjectId = projectId;
     }
 
+    /**
+     * Get the project id to which the time is connected to.
+     * @return Id for connected project.
+     */
     public long getProjectId()
     {
         return mProjectId;
     }
 
+    /**
+     * Set the start time in milliseconds, represent the date and time (UNIX)
+     * at which the object was considered clocked in.
+     * @param start Start time in milliseconds.
+     */
     public void setStart(long start)
     {
         mStart = start;
     }
 
+    /**
+     * Get the start time in milliseconds, represent the date and time
+     * at which the object was considered clocked in.
+     * @return Start time in milliseconds.
+     */
     public long getStart()
     {
         return mStart;
     }
 
+    /**
+     * Set the stop time in milliseconds, represent the date and time (UNIX)
+     * at which the object was considered clocked out.
+     * @param stop Stop time in milliseconds.
+     * @throws DomainException If the value for stop is less than the value for start.
+     */
     public void setStop(long stop) throws DomainException
     {
+        // Check that the stop value is lager than the start value,
+        // should not be able to clock out before clocked in.
         if (stop < getStart()) {
             throw new DomainException();
         }
@@ -58,16 +84,30 @@ public class Time extends DomainObject
         mStop = stop;
     }
 
+    /**
+     * Get the stop time in milliseconds, represent the date and time (UNIX)
+     * at which the object was considered clocked out.
+     * @return Stop time in milliseconds.
+     */
     public long getStop()
     {
         return mStop;
     }
 
+    /**
+     * Set clock in at given date.
+     * @param date Date to clock in.
+     */
     public void clockInAt(Date date)
     {
         setStart(date.getTime());
     }
 
+    /**
+     * Set clock out at given date.
+     * @param date Date to clock out.
+     * @throws DomainException If clock out date is before clock in date.
+     */
     public void clockOutAt(Date date) throws DomainException
     {
         setStop(date.getTime());
