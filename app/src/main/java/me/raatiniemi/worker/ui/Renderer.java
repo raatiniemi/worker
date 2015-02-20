500db71447d7975b5dd298e256c9d4f67a80aaf1
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-20 21:32:32 +0100

    Renderer - Added abstrat method for creating the view holder.

--- app/src/main/java/me/raatiniemi/worker/ui/Renderer.java
+++ app/src/main/java/me/raatiniemi/worker/ui/Renderer.java
@@ -1,5 +1,8 @@
 package me.raatiniemi.worker.ui;
 
+import android.view.ViewGroup;
+
 public abstract class Renderer
 {
+    public abstract RenderViewHolder onCreateViewHolder(ViewGroup viewGroup);
 }
