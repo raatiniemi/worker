b706bf14c280708584172914e7556578f8b99556
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-20 20:34:04 +0100

    RenderViewHolder - Retrieve the context from the view.

--- app/src/main/java/me/raatiniemi/worker/ui/RenderViewHolder.java
+++ app/src/main/java/me/raatiniemi/worker/ui/RenderViewHolder.java
@@ -1,12 +1,22 @@
 package me.raatiniemi.worker.ui;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public abstract class RenderViewHolder extends RecyclerView.ViewHolder
 {
+    private Context context;
+
     public RenderViewHolder(View view)
     {
         super(view);
+
+        this.context = view.getContext();
+    }
+
+    protected Context getContext()
+    {
+        return this.context;
     }
 }
