0994636db0a4d632f7aa52cb2a89116650157fe3
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-02-22 18:40:27 +0100

    Added boilerplate code to the RenderAdapter.

--- app/src/main/java/me/raatiniemi/worker/renderers/RenderAdapter.java
+++ app/src/main/java/me/raatiniemi/worker/renderers/RenderAdapter.java
@@ -1,5 +1,34 @@
 package me.raatiniemi.worker.renderers;
 
-public class RenderAdapter
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class RenderAdapter extends RecyclerView.Adapter<RenderViewHolder>
 {
+    public RenderAdapter()
+    {
+    }
+
+    @Override
+    public RenderViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RenderViewHolder holder, int position)
+    {
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position)
+    {
+        return super.getItemViewType(position);
+    }
 }
