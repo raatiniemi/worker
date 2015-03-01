23b68ab4e973bc4fb9a0a1bcaedb69988625a400
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-01 19:05:20 +0100

    ProjectListFragment - Implemented the onClockActivityToggle.

--- app/src/main/java/me/raatiniemi/worker/ui/ProjectListFragment.java
+++ app/src/main/java/me/raatiniemi/worker/ui/ProjectListFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import me.raatiniemi.worker.R;
 import me.raatiniemi.worker.domain.Project;
@@ -36,7 +37,7 @@ public class ProjectListFragment extends Fragment
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
 
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.fragment_project_list);
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.fragment_project_list);
         recyclerView.setLayoutManager(layoutManager);
 
         // Instantiate the data mapper for time and project.
@@ -45,7 +46,31 @@ public class ProjectListFragment extends Fragment
         // Retrieve the available projects from the project data mapper.
         ArrayList<Project> projects = projectMapper.getProjects();
 
-        ProjectListAdapter adapter = new ProjectListAdapter(projects);
+        final ProjectListAdapter adapter = new ProjectListAdapter(projects);
+        adapter.setOnClockActivityChangeListener(new ProjectListAdapter.OnClockActivityChangeListener() {
+            @Override
+            public void onClockActivityToggle(View view)
+            {
+                int position = recyclerView.getChildPosition(view);
+                if (RecyclerView.NO_POSITION < position) {
+                    Project project = adapter.get(position);
+                    if (null != project) {
+                        onClockActivityChange(position, project, new Date());
+                    }
+                }
+            }
+
+            @Override
+            public void onClockActivityAt(View view)
+            {
+                // TODO: Implement "onClockActivityAt".
+            }
+        });
         recyclerView.setAdapter(adapter);
+    }
+
+    private void onClockActivityChange(int position, Project project, Date date)
+    {
+        // TODO: Implement "onClockActivityChange".
     }
 }
