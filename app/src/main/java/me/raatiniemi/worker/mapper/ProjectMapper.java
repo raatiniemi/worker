76fa507c8e2aee36bfe7e9aaa08903e7de30cb6c
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-06 04:16:53 +0100

    Migrated the project columns to the WorkerContract-class.

--- app/src/main/java/me/raatiniemi/worker/mapper/ProjectMapper.java
+++ app/src/main/java/me/raatiniemi/worker/mapper/ProjectMapper.java
@@ -2,34 +2,25 @@ package me.raatiniemi.worker.mapper;
 
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.provider.BaseColumns;
 
 import java.util.ArrayList;
 
 import me.raatiniemi.worker.domain.Project;
 import me.raatiniemi.worker.domain.Time;
 import me.raatiniemi.worker.exception.ProjectAlreadyExistsException;
+import me.raatiniemi.worker.provider.WorkerContract.*;
 
 public class ProjectMapper extends AbstractMapper<Project>
 {
     private static final String TABLE_NAME = "project";
 
-    private interface Columns
-    {
-        String NAME = "name";
-
-        String DESCRIPTION = "description";
-
-        String ARCHIVED = "archived";
-    }
-
     public static final String CREATE_TABLE =
         "CREATE TABLE " + TABLE_NAME + " ( " +
-            BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            Columns.NAME + " TEXT NOT NULL, " +
-            Columns.DESCRIPTION + " TEXT NULL, " +
-            Columns.ARCHIVED + " INTEGER DEFAULT 0, " +
-            "UNIQUE (" + Columns.NAME + ") ON CONFLICT ROLLBACK" +
+            ProjectColumns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            ProjectColumns.NAME + " TEXT NOT NULL, " +
+            ProjectColumns.DESCRIPTION + " TEXT NULL, " +
+            ProjectColumns.ARCHIVED + " INTEGER DEFAULT 0, " +
+            "UNIQUE (" + ProjectColumns.NAME + ") ON CONFLICT ROLLBACK" +
         ");";
 
     private TimeMapper mTimeMapper;
@@ -49,19 +40,19 @@ public class ProjectMapper extends AbstractMapper<Project>
     protected String[] getColumns()
     {
         return new String[]{
-            BaseColumns._ID,
-            Columns.NAME,
-            Columns.DESCRIPTION,
-            Columns.ARCHIVED
+            ProjectColumns.ID,
+            ProjectColumns.NAME,
+            ProjectColumns.DESCRIPTION,
+            ProjectColumns.ARCHIVED
         };
     }
 
     protected Project load(Cursor row)
     {
-        long id = row.getLong(row.getColumnIndex(BaseColumns._ID));
-        String name = row.getString(row.getColumnIndex(Columns.NAME));
-        String description = row.getString(row.getColumnIndex(Columns.DESCRIPTION));
-        long archived = row.getLong(row.getColumnIndex(Columns.ARCHIVED));
+        long id = row.getLong(row.getColumnIndex(ProjectColumns.ID));
+        String name = row.getString(row.getColumnIndex(ProjectColumns.NAME));
+        String description = row.getString(row.getColumnIndex(ProjectColumns.DESCRIPTION));
+        long archived = row.getLong(row.getColumnIndex(ProjectColumns.ARCHIVED));
 
         Project project = new Project(id, name);
         project.setDescription(description);
@@ -94,7 +85,7 @@ public class ProjectMapper extends AbstractMapper<Project>
 
     public Project find(String name)
     {
-        String selection = Columns.NAME + "=?";
+        String selection = ProjectColumns.NAME + "=?";
         String[] selectionArgs = new String[]{name};
 
         Cursor row = mDatabase.query(getTable(), getColumns(), selection, selectionArgs, null, null, null);
@@ -112,8 +103,8 @@ public class ProjectMapper extends AbstractMapper<Project>
         }
 
         ContentValues values = new ContentValues();
-        values.put(Columns.NAME, project.getName());
-        values.put(Columns.DESCRIPTION, project.getDescription());
+        values.put(ProjectColumns.NAME, project.getName());
+        values.put(ProjectColumns.DESCRIPTION, project.getDescription());
 
         long id = mDatabase.insert(getTable(), null, values);
         return find(id);
--- app/src/main/java/me/raatiniemi/worker/provider/WorkerContract.java
+++ app/src/main/java/me/raatiniemi/worker/provider/WorkerContract.java
@@ -1,5 +1,17 @@
 package me.raatiniemi.worker.provider;
 
+import android.provider.BaseColumns;
+
 public class WorkerContract
 {
+    public interface ProjectColumns
+    {
+        String ID = BaseColumns._ID;
+
+        String NAME = "name";
+
+        String DESCRIPTION = "description";
+
+        String ARCHIVED = "archived";
+    }
 }
