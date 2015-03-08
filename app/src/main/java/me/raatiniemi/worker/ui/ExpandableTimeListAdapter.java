a37ca69b2f81439f8a1be94d65a775377bed1bb7
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-08 13:54:25 +0100

    ExpandableTimeListAdapter - Extended the AbstractExpandableItemAdapter with default method implementation.

--- app/src/main/java/me/raatiniemi/worker/ui/ExpandableTimeListAdapter.java
+++ app/src/main/java/me/raatiniemi/worker/ui/ExpandableTimeListAdapter.java
@@ -1,14 +1,17 @@
 package me.raatiniemi.worker.ui;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractExpandableItemAdapter;
 import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractExpandableItemViewHolder;
 
 import me.raatiniemi.worker.R;
 
 public class ExpandableTimeListAdapter
+    extends AbstractExpandableItemAdapter<ExpandableTimeListAdapter.GroupViewHolder, ExpandableTimeListAdapter.ChildViewHolder>
 {
     public static class BaseViewHolder extends AbstractExpandableItemViewHolder
     {
@@ -46,5 +49,69 @@ public class ExpandableTimeListAdapter
             mTitle = (TextView) view.findViewById(R.id.fragment_time_list_child_item_title);
             mSummarize = (TextView) view.findViewById(R.id.fragment_time_list_child_item_summarize);
         }
+    }
+
+    @Override
+    public GroupViewHolder onCreateGroupViewHolder(ViewGroup viewGroup, int i)
+    {
+        return null;
+    }
+
+    @Override
+    public void onBindGroupViewHolder(GroupViewHolder groupViewHolder, int i, int i2)
+    {
+    }
+
+    @Override
+    public int getGroupCount()
+    {
+        return 0;
+    }
+
+    @Override
+    public int getGroupItemViewType(int i)
+    {
+        return 0;
+    }
+
+    @Override
+    public long getGroupId(int i)
+    {
+        return 0;
+    }
+
+    @Override
+    public ChildViewHolder onCreateChildViewHolder(ViewGroup viewGroup, int i)
+    {
+        return null;
+    }
+
+    @Override
+    public void onBindChildViewHolder(ChildViewHolder childViewHolder, int i, int i2, int i3)
+    {
+    }
+
+    @Override
+    public int getChildCount(int i)
+    {
+        return 0;
+    }
+
+    @Override
+    public int getChildItemViewType(int i, int i2)
+    {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i2)
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean onCheckCanExpandOrCollapseGroup(GroupViewHolder groupViewHolder, int i, int i2, int i3, boolean b)
+    {
+        return false;
     }
 }
