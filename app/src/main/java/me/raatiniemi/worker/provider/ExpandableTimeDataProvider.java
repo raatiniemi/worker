a4b6924a09697135736de2ee4b9ae1b1a60b195b
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-09 19:51:32 +0100

    Added base implementation for the ExpandableTimeDataProvider.

---
+++ app/src/main/java/me/raatiniemi/worker/provider/ExpandableTimeDataProvider.java
@@ -0,0 +1,28 @@
+package me.raatiniemi.worker.provider;
+
+public class ExpandableTimeDataProvider extends ExpandableDataProvider
+{
+    @Override
+    public int getGroupCount()
+    {
+        return 0;
+    }
+
+    @Override
+    public Group getGroupItem(int groupPosition)
+    {
+        return null;
+    }
+
+    @Override
+    public int getChildCount(int groupPosition)
+    {
+        return 0;
+    }
+
+    @Override
+    public Child getChildItem(int groupPosition, int childPosition)
+    {
+        return null;
+    }
+}
