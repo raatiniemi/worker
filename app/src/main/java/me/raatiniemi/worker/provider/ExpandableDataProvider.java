80bdfc72d7ffed028821138f557b54c2112b7f2b
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-09 19:40:51 +0100

    Added abstract interface for the ExpandableDataProvider.

--- app/src/main/java/me/raatiniemi/worker/provider/ExpandableDataProvider.java
+++ app/src/main/java/me/raatiniemi/worker/provider/ExpandableDataProvider.java
@@ -1,5 +1,39 @@
 package me.raatiniemi.worker.provider;
 
-public class ExpandableDataProvider
+import android.util.Pair;
+
+import java.util.List;
+
+public abstract class ExpandableDataProvider
 {
+    public static class Groupable extends Pair<Group, List<Child>>
+    {
+        public Groupable(Group group, List<Child> children)
+        {
+            super(group, children);
+        }
+    }
+
+    public static abstract class Data
+    {
+        public abstract String getText();
+    }
+
+    public static abstract class Group extends Data
+    {
+        public abstract int getGroupId();
+    }
+
+    public static abstract class Child extends Data
+    {
+        public abstract int getChildId();
+    }
+
+    public abstract int getGroupCount();
+
+    public abstract int getChildCount(int groupPosition);
+
+    public abstract Group getGroupItem(int groupPosition);
+
+    public abstract Child getChildItem(int groupPosition, int childPosition);
 }
