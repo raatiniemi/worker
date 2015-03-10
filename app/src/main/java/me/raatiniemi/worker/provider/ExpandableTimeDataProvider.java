1f4cba630d45aed4fbdb51a0a5acae72fce30fd8
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-10 04:16:00 +0100

    Added additional implementation for TimeGroup and TimeChild.

--- app/src/main/java/me/raatiniemi/worker/provider/ExpandableTimeDataProvider.java
+++ app/src/main/java/me/raatiniemi/worker/provider/ExpandableTimeDataProvider.java
@@ -1,24 +1,57 @@
 package me.raatiniemi.worker.provider;
 
+import java.util.Date;
 import java.util.List;
+
+import me.raatiniemi.worker.domain.Time;
 
 public class ExpandableTimeDataProvider extends ExpandableDataProvider
 {
     public static class TimeGroup extends Group
     {
+        private int mId;
+
+        private Date mDate;
+
+        public TimeGroup(int id, Date date)
+        {
+            mId = id;
+            mDate = date;
+        }
+
         @Override
         public int getGroupId()
         {
-            return 0;
+            return mId;
+        }
+
+        public Date getDate()
+        {
+            return mDate;
         }
     }
 
     public static class TimeChild extends Child
     {
+        private int mId;
+
+        private Time mTime;
+
+        public TimeChild(int id, Time time)
+        {
+            mId = id;
+            mTime = time;
+        }
+
         @Override
         public int getChildId()
         {
-            return 0;
+            return mId;
+        }
+
+        public Time getTime()
+        {
+            return mTime;
         }
     }
 
