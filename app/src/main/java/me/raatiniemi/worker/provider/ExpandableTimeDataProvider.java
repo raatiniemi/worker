3a6940d6124dd0a96b37dc4b7cd111424ccb06fe
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-10 03:22:31 +0100

    Added TimeGroup and TimeChild with base implementation.

--- app/src/main/java/me/raatiniemi/worker/provider/ExpandableTimeDataProvider.java
+++ app/src/main/java/me/raatiniemi/worker/provider/ExpandableTimeDataProvider.java
@@ -2,6 +2,36 @@ package me.raatiniemi.worker.provider;
 
 public class ExpandableTimeDataProvider extends ExpandableDataProvider
 {
+    public static class TimeGroup extends Group
+    {
+        @Override
+        public int getGroupId()
+        {
+            return 0;
+        }
+
+        @Override
+        public String getText()
+        {
+            return null;
+        }
+    }
+
+    public static class TimeChild extends Child
+    {
+        @Override
+        public int getChildId()
+        {
+            return 0;
+        }
+
+        @Override
+        public String getText()
+        {
+            return null;
+        }
+    }
+
     @Override
     public int getGroupCount()
     {
