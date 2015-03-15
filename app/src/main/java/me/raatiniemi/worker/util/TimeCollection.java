fd518415acd7e2be48c292634513c84adb109827
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-15 10:17:43 +0100

    Moved the TimeCollection to the util namespace.

--- app/src/main/java/me/raatiniemi/worker/util/TimeCollection.java
+++ app/src/main/java/me/raatiniemi/worker/util/TimeCollection.java
@@ -1,6 +1,8 @@
-package me.raatiniemi.worker.domain;
+package me.raatiniemi.worker.util;
 
 import java.util.ArrayList;
+
+import me.raatiniemi.worker.domain.Time;
 
 public class TimeCollection extends ArrayList<Time>
 {
