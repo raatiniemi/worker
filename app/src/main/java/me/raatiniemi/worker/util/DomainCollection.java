3c67df3df52556c59a33b6f829e6a116f43fd1af
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-03-15 10:17:00 +0100

    Added the DomainCollection-class.

---
+++ app/src/main/java/me/raatiniemi/worker/util/DomainCollection.java
@@ -0,0 +1,9 @@
+package me.raatiniemi.worker.util;
+
+import java.util.ArrayList;
+
+import me.raatiniemi.worker.domain.DomainObject;
+
+public class DomainCollection<T extends DomainObject> extends ArrayList<T>
+{
+}
