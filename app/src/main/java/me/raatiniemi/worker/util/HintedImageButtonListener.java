94fbfd5ce8d98f6aff953f3e7c48330a3e3f31be
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-04-16 20:06:58 +0200

    HintedImageButtonListener - Documentation.

--- app/src/main/java/me/raatiniemi/worker/util/HintedImageButtonListener.java
+++ app/src/main/java/me/raatiniemi/worker/util/HintedImageButtonListener.java
@@ -6,21 +6,35 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
+/**
+ * Display hint for the image via long click.
+ */
 public class HintedImageButtonListener implements View.OnLongClickListener
 {
     protected Context mContext;
 
+    /**
+     * Construct the hinted image button listener.
+     * @param context Context to use.
+     */
     public HintedImageButtonListener(Context context)
     {
         mContext = context;
     }
 
+    /**
+     * Display the content description of the view in a toast message.
+     * @param view View pressed by the user.
+     * @return True if the long click was consumed, otherwise false.
+     */
     @Override
     public boolean onLongClick(View view)
     {
         if (null != view) {
+            // Check that the view actually have content description to display.
             CharSequence description = view.getContentDescription();
             if (!TextUtils.isEmpty(description)) {
+                // Retrieve the position of the view.
                 int[] position = new int[2];
                 view.getLocationInWindow(position);
 
