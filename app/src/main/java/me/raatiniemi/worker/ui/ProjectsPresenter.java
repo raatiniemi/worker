555481acf79da4b6a4c07212560e715101505991
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-04-19 12:44:12 +0200

    ProjectsPresenter - Added support for attaching/detaching views.

--- app/src/main/java/me/raatiniemi/worker/ui/ProjectsPresenter.java
+++ app/src/main/java/me/raatiniemi/worker/ui/ProjectsPresenter.java
@@ -1,5 +1,28 @@
 package me.raatiniemi.worker.ui;
 
+import android.app.Fragment;
+
 public class ProjectsPresenter
 {
+    private Fragment mFragment;
+
+    private Fragment getView()
+    {
+        return mFragment;
+    }
+
+    private boolean isViewAttached()
+    {
+        return null != getView();
+    }
+
+    public void attachView(Fragment fragment)
+    {
+        mFragment = fragment;
+    }
+
+    public void detachView()
+    {
+        mFragment = null;
+    }
 }
