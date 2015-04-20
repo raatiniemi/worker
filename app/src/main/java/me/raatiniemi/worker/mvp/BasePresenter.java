7045153a223015860df1dd9fecf2dc396b6a37ac
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-04-20 20:43:46 +0200

    BasePresenter - Added empty implementation for MvpPresenter.

--- app/src/main/java/me/raatiniemi/worker/mvp/BasePresenter.java
+++ app/src/main/java/me/raatiniemi/worker/mvp/BasePresenter.java
@@ -1,5 +1,14 @@
 package me.raatiniemi.worker.mvp;
 
-public class BasePresenter
+public class BasePresenter<V extends MvpView> implements MvpPresenter<V>
 {
+    @Override
+    public void attachView(V view)
+    {
+    }
+
+    @Override
+    public void detachView()
+    {
+    }
 }
