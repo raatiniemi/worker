4b8433d06c19d5fb9a011201b852574dc8c9eefe
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-04-20 20:32:57 +0200

    MvpPresenter - Added methods for attaching and detaching the view.

--- app/src/main/java/me/raatiniemi/worker/mvp/MvpPresenter.java
+++ app/src/main/java/me/raatiniemi/worker/mvp/MvpPresenter.java
@@ -1,5 +1,8 @@
 package me.raatiniemi.worker.mvp;
 
-public interface MvpPresenter
+public interface MvpPresenter<V extends MvpView>
 {
+    public void attachView(V view);
+
+    public void detachView();
 }
