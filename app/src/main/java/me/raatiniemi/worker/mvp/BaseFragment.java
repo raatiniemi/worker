895d0bb6fc35cac65fa69457a4706d21a1634650
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-04-22 19:57:07 +0200

    Added documentation to the BaseFragment.

--- app/src/main/java/me/raatiniemi/worker/mvp/BaseFragment.java
+++ app/src/main/java/me/raatiniemi/worker/mvp/BaseFragment.java
@@ -4,10 +4,21 @@ import android.app.Fragment;
 
 abstract public class BaseFragment<P extends MvpPresenter> extends Fragment implements MvpView
 {
+    /**
+     * Instance for the presenter.
+     */
     private P mPresenter;
 
+    /**
+     * Create the instance for the presenter.
+     * @return Instance for the presenter.
+     */
     abstract protected P createPresenter();
 
+    /**
+     * Retrieve the presenter, create instance if none is available.
+     * @return Instance for the presenter.
+     */
     protected P getPresenter()
     {
         if (null == mPresenter) {
