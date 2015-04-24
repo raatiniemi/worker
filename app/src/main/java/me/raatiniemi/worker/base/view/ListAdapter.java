1913003953ede59e145a9ef0983d1dd3528ac4df
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-04-24 04:25:38 +0200

    ListAdapter - Added setter (via constructor) and getter for context.

--- app/src/main/java/me/raatiniemi/worker/base/view/ListAdapter.java
+++ app/src/main/java/me/raatiniemi/worker/base/view/ListAdapter.java
@@ -1,9 +1,21 @@
 package me.raatiniemi.worker.base.view;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
 abstract public class ListAdapter<T extends List> extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 {
+    private Context mContext;
+
+    protected Context getContext()
+    {
+        return mContext;
+    }
+
+    public ListAdapter(Context context)
+    {
+        mContext = context;
+    }
 }
