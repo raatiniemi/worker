5eb479bf278438e22ddba88b10d1fa0c417d5061
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-04-24 04:22:42 +0200

    Added ListViewAdapter-class.

---
+++ app/src/main/java/me/raatiniemi/worker/base/view/ListAdapter.java
@@ -0,0 +1,9 @@
+package me.raatiniemi.worker.base.view;
+
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+abstract public class ListAdapter<T extends List> extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+{
+}
