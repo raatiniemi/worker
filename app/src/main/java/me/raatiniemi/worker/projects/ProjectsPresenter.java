de4317b82dbcb46260f7f80141bd75622b81e2f6
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-05-20 04:57:15 +0200

    ProjectsPresenter - Documentation.

--- app/src/main/java/me/raatiniemi/worker/projects/ProjectsPresenter.java
+++ app/src/main/java/me/raatiniemi/worker/projects/ProjectsPresenter.java
@@ -6,6 +6,9 @@ import me.raatiniemi.worker.base.presenter.BasePresenter;
 import me.raatiniemi.worker.mapper.ProjectMapper;
 import me.raatiniemi.worker.util.ProjectCollection;
 
+/**
+ * Presenter for the projects module, handles loading of projects.
+ */
 public class ProjectsPresenter extends BasePresenter<ProjectsFragment> {
     private AsyncTask<ProjectMapper, Void, ProjectCollection> mProjectLoader;
 
@@ -13,12 +16,20 @@ public class ProjectsPresenter extends BasePresenter<ProjectsFragment> {
     public void detachView() {
         super.detachView();
 
+        // Before we can detach the view we have to clean up.
         if (null != mProjectLoader && !mProjectLoader.isCancelled()) {
             mProjectLoader.cancel(true);
         }
     }
 
+    /**
+     * Load the projects from the mapper and populate the view.
+     *
+     * @param mapper Mapper to use for the data loading.
+     */
     public void loadProjects(ProjectMapper mapper) {
+        // If the project loader is already active we have to cancel
+        // its running process before we can start a new process.
         if (null != mProjectLoader && !mProjectLoader.isCancelled()) {
             mProjectLoader.cancel(true);
         }
@@ -38,10 +49,13 @@ public class ProjectsPresenter extends BasePresenter<ProjectsFragment> {
 
             @Override
             protected void onPostExecute(ProjectCollection projects) {
+                // If the process have been cancelled or projects have not
+                // been initialized (no mapper available), we have to abort.
                 if (isCancelled() || null == projects) {
                     return;
                 }
 
+                // If the view is attached, populate it with the retrieved projects.
                 if (isViewAttached()) {
                     getView().setData(projects);
                 }
