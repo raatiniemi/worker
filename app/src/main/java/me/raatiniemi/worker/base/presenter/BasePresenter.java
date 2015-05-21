8573ccb2cf021af1c6c6830db44c2d04aeff6c00
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-05-21 05:11:17 +0200

    Presenter - Documentation.

--- app/src/main/java/me/raatiniemi/worker/base/presenter/BasePresenter.java
+++ app/src/main/java/me/raatiniemi/worker/base/presenter/BasePresenter.java
@@ -2,22 +2,48 @@ package me.raatiniemi.worker.base.presenter;
 
 import me.raatiniemi.worker.base.view.MvpView;
 
+/**
+ * Base presenter with basic functionality.
+ *
+ * @param <V> Reference type for the view attached to the presenter.
+ */
 public class BasePresenter<V extends MvpView> implements MvpPresenter<V> {
+    /**
+     * View attached to the presenter.
+     */
     private V mView;
 
+    /**
+     * Retrieve the view attached to the presenter.
+     *
+     * @return Attached view if available, otherwise null.
+     */
     protected V getView() {
         return mView;
     }
 
+    /**
+     * Check whether a view is attached to the presenter.
+     *
+     * @return True if a view is attached, otherwise false.
+     */
     protected boolean isViewAttached() {
         return null != getView();
     }
 
+    /**
+     * Attach a view to the presenter.
+     *
+     * @param view View to attach to the presenter.
+     */
     @Override
     public void attachView(V view) {
         mView = view;
     }
 
+    /**
+     * Detach the view from the presenter.
+     */
     @Override
     public void detachView() {
         mView = null;
--- app/src/main/java/me/raatiniemi/worker/base/presenter/MvpPresenter.java
+++ app/src/main/java/me/raatiniemi/worker/base/presenter/MvpPresenter.java
@@ -2,8 +2,21 @@ package me.raatiniemi.worker.base.presenter;
 
 import me.raatiniemi.worker.base.view.MvpView;
 
+/**
+ * Interface for the presenter.
+ *
+ * @param <V> Reference type for the view attached to the presenter.
+ */
 public interface MvpPresenter<V extends MvpView> {
+    /**
+     * Attach a view to the presenter.
+     *
+     * @param view View to attach to the presenter.
+     */
     void attachView(V view);
 
+    /**
+     * Detach the view from the presenter.
+     */
     void detachView();
 }
