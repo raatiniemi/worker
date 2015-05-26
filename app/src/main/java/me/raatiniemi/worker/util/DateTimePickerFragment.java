7217ea7bc52a02bdde60bd96009f233f2cb66c25
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-05-26 19:28:07 +0200

    DateTimePickerFragment - Added structure for date and time listener.

--- app/src/main/java/me/raatiniemi/worker/util/DateTimePickerFragment.java
+++ app/src/main/java/me/raatiniemi/worker/util/DateTimePickerFragment.java
@@ -1,4 +1,18 @@
 package me.raatiniemi.worker.util;
 
-public class DateTimePickerFragment {
+import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.app.TimePickerDialog;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+public class DateTimePickerFragment extends Fragment
+    implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+    }
 }
