72e172f110c6386c581b62563732108a8f85fde0
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-05-27 04:30:06 +0200

    DateTimerPickerFragment - Added getter/setter for the minimum date.

--- app/src/main/java/me/raatiniemi/worker/util/DateTimePickerFragment.java
+++ app/src/main/java/me/raatiniemi/worker/util/DateTimePickerFragment.java
@@ -6,8 +6,33 @@ import android.app.TimePickerDialog;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
+import java.util.Calendar;
+
 public class DateTimePickerFragment extends Fragment
     implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+    /**
+     * Minimum date available for the date picker.
+     */
+    private Calendar mMinDate;
+
+    /**
+     * Retrieve the minimum date available for the date picker.
+     *
+     * @return Minimum date, or null if none is set.
+     */
+    public Calendar getMinDate() {
+        return mMinDate;
+    }
+
+    /**
+     * Set the minimum date for the date picker.
+     *
+     * @param minDate Minimum date.
+     */
+    public void setMinDate(Calendar minDate) {
+        mMinDate = minDate;
+    }
+
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
     }
