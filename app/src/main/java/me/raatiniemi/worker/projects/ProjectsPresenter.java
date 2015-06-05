8b0cce8ae2d201acb75bebbff6c486672e6eaf13
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-06-05 22:41:54 +0200

    ProjectsPresenter - Refactor project loading to use AsyncTaskResult.

--- app/src/main/java/me/raatiniemi/worker/projects/ProjectsPresenter.java
+++ app/src/main/java/me/raatiniemi/worker/projects/ProjectsPresenter.java
@@ -4,13 +4,14 @@ import android.os.AsyncTask;
 
 import me.raatiniemi.worker.base.presenter.BasePresenter;
 import me.raatiniemi.worker.mapper.ProjectMapper;
+import me.raatiniemi.worker.util.AsyncTaskResult;
 import me.raatiniemi.worker.util.ProjectCollection;
 
 /**
  * Presenter for the projects module, handles loading of projects.
  */
 public class ProjectsPresenter extends BasePresenter<ProjectsFragment> {
-    private AsyncTask<ProjectMapper, Void, ProjectCollection> mProjectLoader;
+    private AsyncTask<ProjectMapper, Void, AsyncTaskResult<ProjectCollection>> mProjectLoader;
 
     @Override
     public void detachView() {
@@ -35,30 +36,36 @@ public class ProjectsPresenter extends BasePresenter<ProjectsFragment> {
             mProjectLoader.cancel(true);
         }
 
-        mProjectLoader = new AsyncTask<ProjectMapper, Void, ProjectCollection>() {
+        mProjectLoader = new AsyncTask<ProjectMapper, Void, AsyncTaskResult<ProjectCollection>>() {
             @Override
-            protected ProjectCollection doInBackground(ProjectMapper... params) {
+            protected AsyncTaskResult<ProjectCollection> doInBackground(ProjectMapper... params) {
                 // Check that we have received the project mapper as argument.
                 if (0 == params.length || null == params[0]) {
                     return null;
                 }
 
-                // Retrieve the projects from the project mapper.
-                ProjectMapper mapper = params[0];
-                return mapper.getProjects();
+                AsyncTaskResult<ProjectCollection> result;
+                try {
+                    // Retrieve the projects from the project mapper.
+                    ProjectMapper mapper = params[0];
+                    result = new AsyncTaskResult<>(mapper.getProjects());
+                } catch (Throwable e) {
+                    result = new AsyncTaskResult<>(e);
+                }
+                return result;
             }
 
             @Override
-            protected void onPostExecute(ProjectCollection projects) {
-                // If the process have been cancelled or projects have not
+            protected void onPostExecute(AsyncTaskResult<ProjectCollection> result) {
+                // If the process have been cancelled or result have not
                 // been initialized (no mapper available), we have to abort.
-                if (isCancelled() || null == projects) {
+                if (isCancelled() || null == result) {
                     return;
                 }
 
-                // If the view is attached, populate it with the retrieved projects.
+                // If the view is attached, populate it with the retrieved data.
                 if (isViewAttached()) {
-                    getView().setData(projects);
+                    getView().setData(result.getData());
                 }
             }
         };
