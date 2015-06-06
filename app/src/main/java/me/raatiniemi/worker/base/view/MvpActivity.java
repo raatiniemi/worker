aa3af73c4dc36e3dd40f7347a120bf95f07bf323
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-06-06 06:39:28 +0200

    Added the MvpActivity-class, duplicated from the MvpFragment-class.

---
+++ app/src/main/java/me/raatiniemi/worker/base/view/MvpActivity.java
@@ -0,0 +1,44 @@
+package me.raatiniemi.worker.base.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+import me.raatiniemi.worker.base.presenter.MvpPresenter;
+
+abstract public class MvpActivity<P extends MvpPresenter> extends AppCompatActivity implements MvpView {
+    /**
+     * Instance for the presenter.
+     */
+    private P mPresenter;
+
+    /**
+     * Create the instance for the presenter.
+     *
+     * @return Instance for the presenter.
+     */
+    abstract protected P createPresenter();
+
+    /**
+     * Retrieve the presenter, create instance if none is available.
+     *
+     * @return Instance for the presenter.
+     */
+    protected P getPresenter() {
+        if (null == mPresenter) {
+            mPresenter = createPresenter();
+        }
+        return mPresenter;
+    }
+
+    /**
+     * Handles clean up when the activity is destroyed.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // If the presenter is still active, we have to detach it.
+        if (null != getPresenter()) {
+            getPresenter().detachView();
+        }
+    }
+}
