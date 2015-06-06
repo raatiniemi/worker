42c030f4a9eedf576c6326b6edf17ad4b7bb2d04
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-06-06 06:10:48 +0200

    ProjectsPresenter - Simplified and cleaned up declaration for ProjectsReadTask.

--- app/src/main/java/me/raatiniemi/worker/projects/ProjectsPresenter.java
+++ app/src/main/java/me/raatiniemi/worker/projects/ProjectsPresenter.java
@@ -1,28 +1,24 @@
 package me.raatiniemi.worker.projects;
 
-import android.os.AsyncTask;
-
 import me.raatiniemi.worker.base.presenter.BasePresenter;
 import me.raatiniemi.worker.mapper.ProjectMapper;
 import me.raatiniemi.worker.projects.task.ProjectsReadTask;
-import me.raatiniemi.worker.util.AsyncTaskResult;
-import me.raatiniemi.worker.util.ProjectCollection;
 
 /**
  * Presenter for the projects module, handles loading of projects.
  */
 public class ProjectsPresenter extends BasePresenter<ProjectsFragment> {
-    private AsyncTask<ProjectMapper, Void, AsyncTaskResult<ProjectCollection>> mProjectLoader;
+    private ProjectsReadTask mProjectReadTask;
 
     @Override
     public void detachView() {
         super.detachView();
 
         // Before we can detach the view we have to clean up.
-        if (null != mProjectLoader && !mProjectLoader.isCancelled()) {
-            mProjectLoader.cancel(true);
+        if (null != mProjectReadTask && !mProjectReadTask.isCancelled()) {
+            mProjectReadTask.cancel(true);
         }
-        mProjectLoader = null;
+        mProjectReadTask = null;
     }
 
     /**
@@ -33,11 +29,11 @@ public class ProjectsPresenter extends BasePresenter<ProjectsFragment> {
     public void loadProjects(ProjectMapper mapper) {
         // If the project loader is already active we have to cancel
         // its running process before we can start a new process.
-        if (null != mProjectLoader && !mProjectLoader.isCancelled()) {
-            mProjectLoader.cancel(true);
+        if (null != mProjectReadTask && !mProjectReadTask.isCancelled()) {
+            mProjectReadTask.cancel(true);
         }
 
-        mProjectLoader = new ProjectsReadTask(this);
-        mProjectLoader.execute(mapper);
+        mProjectReadTask = new ProjectsReadTask(this);
+        mProjectReadTask.execute(mapper);
     }
 }
