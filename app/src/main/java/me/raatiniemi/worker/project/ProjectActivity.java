33e235b0de930732107b8fbf7c32b2d44668a7ff
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-06-06 09:20:20 +0200

    Added layout for the ProjectActivity.

--- app/src/main/java/me/raatiniemi/worker/project/ProjectActivity.java
+++ app/src/main/java/me/raatiniemi/worker/project/ProjectActivity.java
@@ -1,6 +1,14 @@
 package me.raatiniemi.worker.project;
 
+import android.os.Bundle;
+
+import me.raatiniemi.worker.R;
 import me.raatiniemi.worker.base.view.BaseActivity;
 
 public class ProjectActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project);
+    }
 }
