cc6d1691f66f84c93283ba49862946eeec14d463
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-06-24 20:10:04 +0200

    ProjectsPresenter - Additional documentation and logging.

--- app/src/main/java/me/raatiniemi/worker/projects/ProjectsPresenter.java
+++ app/src/main/java/me/raatiniemi/worker/projects/ProjectsPresenter.java
@@ -1,6 +1,7 @@
 package me.raatiniemi.worker.projects;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.util.Date;
 
@@ -19,6 +20,9 @@ public class ProjectsPresenter extends RxPresenter<ProjectsFragment> {
      */
     private static final String TAG = "ProjectsPresenter";
 
+    /**
+     * Provider for working with projects.
+     */
     private ProjectProvider mProvider;
 
     /**
@@ -33,35 +37,49 @@ public class ProjectsPresenter extends RxPresenter<ProjectsFragment> {
     }
 
     /**
-     * Retrieve the projects.
+     * Retrieve the projects and push them to the view.
      */
     public void getProjects() {
+        // Before we setup the project subscription we have to cancel
+        // the previous one, if available.
         unsubscribe();
 
+        // Setup the subscription for retrieving projects.
         mSubscription = mProvider.getProjects()
             .compose(this.<ProjectCollection>applySchedulers())
             .subscribe(new Action1<ProjectCollection>() {
                 @Override
                 public void call(ProjectCollection projects) {
+                    // Check that we still have the view attached.
                     if (!isViewAttached()) {
+                        Log.d(TAG, "View is not attached, skip pushing projects");
                         return;
                     }
 
+                    // Push the data to the view.
                     getView().setData(projects);
                 }
             });
     }
 
+    /**
+     * Create a new project and update the view.
+     *
+     * @param project Project to create.
+     */
     public void createNewProject(Project project) {
         mProvider.createProject(project)
             .compose(this.<Project>applySchedulers())
             .subscribe(new Action1<Project>() {
                 @Override
                 public void call(Project project) {
+                    // Check that we still have the view attached.
                     if (!isViewAttached()) {
+                        Log.d(TAG, "View is not attached, skip pushing new project");
                         return;
                     }
 
+                    // Add the new project to the view.
                     getView().addProject(project);
                 }
             }, new Action1<Throwable>() {
@@ -73,16 +91,25 @@ public class ProjectsPresenter extends RxPresenter<ProjectsFragment> {
             });
     }
 
+    /**
+     * Change the clock activity status for project, i.e. clock in/out.
+     *
+     * @param project Project to change clock activity status.
+     * @param date Date and time to use for the clock activity change.
+     */
     public void clockActivityChange(Project project, Date date) {
         mProvider.clockActivityChange(project, date)
             .compose(this.<Project>applySchedulers())
             .subscribe(new Action1<Project>() {
                 @Override
                 public void call(Project project) {
+                    // Check that we still have the view attached.
                     if (!isViewAttached()) {
+                        Log.d(TAG, "View is not attached, skip updating project");
                         return;
                     }
 
+                    // Update the project.
                     getView().updateProject(project);
                 }
             }, new Action1<Throwable>() {
