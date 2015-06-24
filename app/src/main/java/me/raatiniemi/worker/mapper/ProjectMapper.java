1c5b9e162f22ac4b6d3fef4cf41c0bd98ee74092
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-06-24 16:37:49 +0200

    ProjectMapper - Removed unnecessary code.
    Removed the inheritance from the AbstractMapper and unused methods.
    
    The ProjectMapper should only be used for mapping between Project
    and other types, e.g. database cursor, content values.
    
    Also, made the constructor private to prevent instantiation.

--- app/src/main/java/me/raatiniemi/worker/mapper/ProjectMapper.java
+++ app/src/main/java/me/raatiniemi/worker/mapper/ProjectMapper.java
@@ -1,103 +1,16 @@
 package me.raatiniemi.worker.mapper;
 
 import android.content.ContentValues;
-import android.content.Context;
 import android.database.Cursor;
 
 import me.raatiniemi.worker.model.project.Project;
-import me.raatiniemi.worker.model.project.ProjectCollection;
 import me.raatiniemi.worker.provider.WorkerContract.ProjectColumns;
-import me.raatiniemi.worker.provider.WorkerContract.ProjectContract;
-import me.raatiniemi.worker.provider.WorkerContract.Tables;
 
-public class ProjectMapper extends AbstractMapper<Project> {
-    private static final String TAG = "ProjectMapper";
-
-    private Context mContext;
-
-    private TimeMapper mTimeMapper;
-
-    public ProjectMapper(Context context, TimeMapper timeMapper) {
-        super();
-
-        mContext = context;
-        mTimeMapper = timeMapper;
-    }
-
+public class ProjectMapper {
     /**
-     * Retrieve the name of the project table within the database.
-     *
-     * @return Name of project table.
+     * Private constructor, instantiation is not allowed.
      */
-    protected String getTable() {
-        return Tables.PROJECT;
-    }
-
-    /**
-     * Load the project object from the cursor.
-     *
-     * @param row Database cursor.
-     * @return Project with data.
-     */
-    protected Project load(Cursor row) {
-        return map(row);
-    }
-
-    /**
-     * Load the time for the project.
-     *
-     * @param project Project for which to load the time.
-     */
-    private void loadTime(Project project) {
-        // If the mapper for time objects is available, we should load
-        // the the project time for the default interval.
-        if (null != mTimeMapper) {
-            project.addTime(mTimeMapper.findTimeByProject(project));
-        }
-    }
-
-
-    /**
-     * Retrieve all of the available projects.
-     *
-     * @return List of all available projects.
-     */
-    public ProjectCollection getProjects() {
-        ProjectCollection result = new ProjectCollection();
-
-        // Exclude projects that have been archived.
-        String selection = "COALESCE(" + ProjectColumns.ARCHIVED + ", 0) = 0";
-        Cursor rows = mContext.getContentResolver().query(ProjectContract.getStreamUri(), ProjectContract.COLUMNS, selection, null, null);
-        if (rows.moveToFirst()) {
-            do {
-                Project project = load(rows);
-
-                // Populate the project with the registered time.
-                loadTime(project);
-                result.add(project);
-            } while (rows.moveToNext());
-        }
-        rows.close();
-
-        return result;
-    }
-
-    public Project find(long id) {
-        Project project = null;
-
-        Cursor row = mContext.getContentResolver().query(
-            ProjectContract.getItemUri(String.valueOf(id)),
-            ProjectContract.COLUMNS,
-            null,
-            null,
-            null
-        );
-        if (row.moveToFirst()) {
-            project = load(row);
-        }
-        row.close();
-
-        return project;
+    private ProjectMapper() {
     }
 
     /**
