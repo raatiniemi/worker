19655f61ff9410c5dcd9367e8e387dcbed007c0d
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-06-29 06:05:07 +0200

    ProjectsView - Added documentation for the methods.

--- app/src/main/java/me/raatiniemi/worker/projects/ProjectsView.java
+++ app/src/main/java/me/raatiniemi/worker/projects/ProjectsView.java
@@ -6,9 +6,22 @@ import me.raatiniemi.worker.model.project.Project;
  * Methods related to handling of the project view.
  */
 public interface ProjectsView {
-    void createNewProject();
-
+    /**
+     * Add project to the list.
+     *
+     * @param project Project to add to the list.
+     */
     void addProject(Project project);
 
+    /**
+     * Update project in the list.
+     *
+     * @param project Project to update in the list.
+     */
     void updateProject(Project project);
+
+    /**
+     * Open the dialog for creating a new project.
+     */
+    void createNewProject();
 }
