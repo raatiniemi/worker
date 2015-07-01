04b052d30b25514217a438b1d18b5c41df9bdea4
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-07-01 18:29:45 +0200

    TimesheetFragment - Removed unused code.

--- app/src/main/java/me/raatiniemi/worker/project/timesheet/TimesheetFragment.java
+++ app/src/main/java/me/raatiniemi/worker/project/timesheet/TimesheetFragment.java
@@ -1,9 +1,7 @@
 package me.raatiniemi.worker.project.timesheet;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -208,21 +206,6 @@ public class TimesheetFragment extends MvpFragment<TimesheetPresenter, List<Grou
         if (timeMapper.remove(time)) {
             Log.d(TAG, "Removing item: " + groupPosition + ":" + childPosition);
             mTimesheetAdapter.remove(groupPosition, childPosition);
-
-            // Create the intent for the broadcast to update the
-            // project list. We have to supply the project id,
-            // otherwise we're unable to properly update the view.
-            //
-            // TODO: Properly name the broadcast intent.
-            // The name of the broadcast intent should be supplied
-            // by the fragment we're trying to update, in case we'd
-            // want to update the fragment from an additional location.
-            Intent intent = new Intent("project-list-view-update");
-            intent.putExtra("project_id", getProjectId());
-
-            // Send the project update broadcast.
-            LocalBroadcastManager.getInstance(getActivity())
-                .sendBroadcast(intent);
         }
     }
 
