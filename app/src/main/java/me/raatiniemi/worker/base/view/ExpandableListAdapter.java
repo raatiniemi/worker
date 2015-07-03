d990a6a1711cb2972241560ff40b22aba4632936
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-07-03 06:10:38 +0200

    Added ExpandableListAdapter-class.

---
+++ app/src/main/java/me/raatiniemi/worker/base/view/ExpandableListAdapter.java
@@ -0,0 +1,12 @@
+package me.raatiniemi.worker.base.view;
+
+import android.support.v7.widget.RecyclerView.ViewHolder;
+
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractExpandableItemAdapter;
+
+abstract public class ExpandableListAdapter<
+    GVH extends ViewHolder, // View holder for the group item
+    CVH extends ViewHolder // View holder for the child item
+    >
+    extends AbstractExpandableItemAdapter<GVH, CVH> {
+}
