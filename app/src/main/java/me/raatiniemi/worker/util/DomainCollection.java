b9988374b5070a4bcb7371a59821a9546de1cd11
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-07-03 14:03:09 +0200

    Removed the DomainCollection-class, no longer used.

--- app/src/main/java/me/raatiniemi/worker/util/DomainCollection.java
+++
@@ -1,8 +0,0 @@
-package me.raatiniemi.worker.util;
-
-import java.util.ArrayList;
-
-import me.raatiniemi.worker.domain.DomainObject;
-
-public class DomainCollection<T extends DomainObject> extends ArrayList<T> {
-}
