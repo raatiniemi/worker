aae423ce8a33bf45548d6932ad53a27307ad87b8
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-07-04 07:09:23 +0200

    WorkerDatabase - Removed singleton method, no longer needed.

--- app/src/main/java/me/raatiniemi/worker/provider/WorkerDatabase.java
+++ app/src/main/java/me/raatiniemi/worker/provider/WorkerDatabase.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import me.raatiniemi.worker.application.Worker;
 import me.raatiniemi.worker.provider.WorkerContract.ProjectColumns;
 import me.raatiniemi.worker.provider.WorkerContract.Tables;
 import me.raatiniemi.worker.provider.WorkerContract.TimeColumns;
@@ -24,29 +23,12 @@ public class WorkerDatabase extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     /**
-     * Instance for the database helper.
-     */
-    private static WorkerDatabase mWorkerDatabase;
-
-    /**
      * Instantiate the database helper with the application context.
      *
      * @param context Application context.
      */
     public WorkerDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    /**
-     * Retrieve the instance for the database helper, instantiate if necessary.
-     *
-     * @return Instance for the database helper.
-     */
-    public static synchronized WorkerDatabase getInstance() {
-        if (null == mWorkerDatabase) {
-            mWorkerDatabase = new WorkerDatabase(Worker.getContext());
-        }
-        return mWorkerDatabase;
     }
 
     /**
