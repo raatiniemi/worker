5cae08529a7cca33ac4eca75d824ed604c9708ab
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-07-08 08:03:41 +0200

    Added the base for displaying the data settings.

--- app/src/main/java/me/raatiniemi/worker/settings/SettingsActivity.java
+++ app/src/main/java/me/raatiniemi/worker/settings/SettingsActivity.java
@@ -1,6 +1,100 @@
 package me.raatiniemi.worker.settings;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import me.raatiniemi.worker.R;
 import me.raatiniemi.worker.base.view.BaseActivity;
 
 public class SettingsActivity extends BaseActivity {
+    /**
+     * Tag for logging.
+     */
+    private static final String TAG = "SettingsActivity";
+
+    /**
+     * Instance for the SettingsActivity.
+     */
+    private static SettingsActivity sInstance;
+
+    /**
+     * Retrieve the instance for the SettingsActivity.
+     *
+     * @return Instance for the SettingsActivity.
+     */
+    static SettingsActivity getInstance() {
+        return sInstance;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Store the instance in a static variable,
+        // so it's accessible from the SettingsFragment.
+        sInstance = this;
+
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new SettingsFragment())
+                .commit();
+        }
+    }
+
+    /**
+     * Switch the currently displayed preference screen.
+     *
+     * @param key Key for the new preference screen.
+     */
+    void switchPreferenceScreen(String key) {
+        // TODO: Actually switch the preference screen.
+        Log.d(TAG, "Switching to preference screen: " + key);
+
+        Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT)
+            .show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fragmentManager = getFragmentManager();
+
+        // Depending on which fragment is contained within the
+        // container, the back button will behave differently.
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        Class<SettingsFragment> settings = SettingsFragment.class;
+        if (!settings.equals(fragment.getClass())) {
+            fragmentManager.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public static class BasePreferenceFragment extends PreferenceFragment {
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, @NonNull Preference preference) {
+            super.onPreferenceTreeClick(preferenceScreen, preference);
+            if (preference instanceof PreferenceScreen) {
+                getInstance().switchPreferenceScreen(preference.getKey());
+            }
+            return false;
+        }
+    }
+
+    public static class SettingsFragment extends BasePreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            getActivity().setTitle(R.string.settings_category_preferences);
+            addPreferencesFromResource(R.xml.settings);
+        }
+    }
 }
