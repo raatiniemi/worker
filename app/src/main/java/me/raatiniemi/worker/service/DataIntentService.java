aaad7c9e02fdd00023f4b8e6a481408e1887353c
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-07-10 09:02:11 +0200

    DataIntentService - Backup the database to the external storage.

--- app/src/main/java/me/raatiniemi/worker/service/DataIntentService.java
+++ app/src/main/java/me/raatiniemi/worker/service/DataIntentService.java
@@ -4,6 +4,13 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import me.raatiniemi.worker.util.ExternalStorage;
+import me.raatiniemi.worker.util.FileUtils;
+
 /**
  * Service for running data operations.
  */
@@ -66,8 +73,35 @@ public class DataIntentService extends IntentService {
      * Run the backup operation. Copies the SQLite database to the backup
      * directory on the external storage.
      */
-    private void backup() {
-        // TODO: Run the backup operation.
+    private synchronized void backup() {
+        // Check that the external storage is writable.
+        if (!ExternalStorage.isWritable()) {
+            Log.w(TAG, "Unable to backup, external storage is not writable");
+            return;
+        }
+
+        // Build the backup folder name with the current timestamp to prevent
+        // running multiple backups against the same directory, which would
+        // effectively override any previous backups.
+        String backupName = "backup-" + (new Date()).getTime();
+        File directory = ExternalStorage.getBackupDirectory(backupName);
+
+        // Check that the backup directory is available.
+        if (null == directory) {
+            Log.w(TAG, "Directory for backup is not available");
+            return;
+        }
+
+        try {
+            // Retrieve the source and destination file locations.
+            File from = getDatabasePath("worker");
+            File to = new File(directory, "worker");
+
+            // Perform the file copy.
+            FileUtils.copy(from, to);
+        } catch (IOException e) {
+            Log.w(TAG, "Unable to backup: " + e.getMessage());
+        }
     }
 
     /**
