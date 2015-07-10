05faec205d95fd75e8445c9e96e2a4ee43a1ff2f
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-07-10 08:38:26 +0200

    DataIntentService - Added the base for running data operations.

--- app/src/main/java/me/raatiniemi/worker/service/DataIntentService.java
+++ app/src/main/java/me/raatiniemi/worker/service/DataIntentService.java
@@ -2,15 +2,79 @@ package me.raatiniemi.worker.service;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
+/**
+ * Service for running data operations.
+ */
 public class DataIntentService extends IntentService {
+    /**
+     * Intent action for running the backup operation.
+     */
+    public static final String INTENT_ACTION_BACKUP = "backup";
+
+    /**
+     * Tag for logging.
+     */
     private static final String TAG = "DataIntentService";
 
+    /**
+     * Type of running data operation.
+     */
+    private static RUNNING sRunning = RUNNING.NONE;
+
+    /**
+     * Constructor.
+     */
     public DataIntentService() {
         super(TAG);
     }
 
+    /**
+     * Get the type of data operation that is running. If the RUNNING.NONE is
+     * returned no data operation is currently running.
+     *
+     * @return Type of running data operation.
+     */
+    public static RUNNING getRunning() {
+        return sRunning;
+    }
+
     @Override
     protected void onHandleIntent(Intent intent) {
+        // If an operation is already running, we should not allow another to
+        // start. We wouldn't want backup and restore running simultaneously.
+        if (RUNNING.NONE != getRunning()) {
+            Log.w(TAG, "Data operation is already running, exiting");
+            return;
+        }
+
+        String action = intent.getAction();
+        switch (action) {
+            case INTENT_ACTION_BACKUP:
+                sRunning = RUNNING.BACKUP;
+                backup();
+                break;
+            default:
+                Log.w(TAG, "Received unknown action: " + action);
+                break;
+        }
+        sRunning = RUNNING.NONE;
+    }
+
+    /**
+     * Run the backup operation. Copies the SQLite database to the backup
+     * directory on the external storage.
+     */
+    private void backup() {
+        // TODO: Run the backup operation.
+    }
+
+    /**
+     * Available types of runnable data operations.
+     */
+    public enum RUNNING {
+        NONE,
+        BACKUP
     }
 }
