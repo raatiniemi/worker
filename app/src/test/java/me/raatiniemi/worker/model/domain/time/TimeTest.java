0a84c7cc3dfebf1a350a4a25d7418f7a5ec13756
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-08-10 19:37:59 +0200

    Additional unit tests for getter and setter methods
    Added additional unit tests for the constructor, getter and setter
    methods of the Time domain model to ensure a consistent behaviour.

--- app/src/test/java/me/raatiniemi/worker/model/domain/time/TimeTest.java
+++ app/src/test/java/me/raatiniemi/worker/model/domain/time/TimeTest.java
@@ -12,6 +12,7 @@ import me.raatiniemi.worker.exception.domain.ClockOutBeforeClockInException;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -19,6 +20,118 @@ import static org.mockito.Mockito.when;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class TimeTest {
+    @Test
+    public void Time_DefaultValueFromDefaultConstructor_True() {
+        Time time = new Time();
+
+        assertNull(time.getId());
+        assertNull(time.getProjectId());
+        assertEquals(Long.valueOf(0L), time.getStart());
+        assertEquals(Long.valueOf(0L), time.getStop());
+    }
+
+    @Test
+    public void Time_DefaultValueFromConstructor_True()
+        throws ClockOutBeforeClockInException {
+        Time time = new Time(1L, 2L, 3L, 4L);
+
+        assertEquals(Long.valueOf(1L), time.getId());
+        assertEquals(Long.valueOf(2L), time.getProjectId());
+        assertEquals(Long.valueOf(3L), time.getStart());
+        assertEquals(Long.valueOf(4L), time.getStop());
+    }
+
+    @Test
+    public void getProjectId_DefaultValue_Null() {
+        Time time = new Time();
+
+        assertNull(time.getProjectId());
+    }
+
+    @Test
+    public void getProjectId_ValueFromConstructor_True()
+        throws ClockOutBeforeClockInException {
+        Time time = new Time(1L, 2L, 3L, 4L);
+
+        assertEquals(Long.valueOf(2L), time.getProjectId());
+
+        time = new Time(3L);
+
+        assertEquals(Long.valueOf(3L), time.getProjectId());
+    }
+
+    @Test
+    public void getProjectId_ValueFromSetter_True() {
+        Time time = new Time();
+        time.setProjectId(1L);
+
+        assertEquals(Long.valueOf(1L), time.getProjectId());
+    }
+
+    @Test
+    public void getStart_DefaultValue_True() {
+        Time time = new Time();
+
+        assertEquals(Long.valueOf(0L), time.getStop());
+    }
+
+    @Test
+    public void getStart_ValueFromConstructor_True()
+        throws ClockOutBeforeClockInException {
+        Time time = new Time(1L, 2L, 3L, 4L);
+
+        assertEquals(Long.valueOf(3L), time.getStart());
+    }
+
+    @Test
+    public void getStart_ValueFromSetter_True()
+        throws ClockOutBeforeClockInException {
+        Time time = new Time();
+        time.setStart(1L);
+
+        assertEquals(Long.valueOf(1L), time.getStart());
+    }
+
+    @Test(expected = ClockOutBeforeClockInException.class)
+    public void getStart_StopLessThanStart_ThrowException()
+        throws ClockOutBeforeClockInException {
+        Time time = new Time();
+        time.setStop(1L);
+        time.setStart(10L);
+    }
+
+    @Test
+    public void getStop_DefaultValue_True() {
+        Time time = new Time();
+
+        assertEquals(Long.valueOf(0L), time.getStop());
+    }
+
+    @Test
+    public void getStop_ValueFromConstructor_True()
+        throws ClockOutBeforeClockInException {
+        Time time = new Time(1L, 2L, 3L, 4L);
+
+        assertEquals(Long.valueOf(4L), time.getStop());
+    }
+
+    @Test
+    public void getStop_ValueFromSetter_True()
+        throws ClockOutBeforeClockInException {
+        Time time = new Time();
+        time.setStop(1L);
+
+        assertEquals(Long.valueOf(1L), time.getStop());
+    }
+
+    @Test(expected = ClockOutBeforeClockInException.class)
+    public void setStop_StopLessThanStart_ThrowException()
+        throws ClockOutBeforeClockInException {
+        Time time = new Time();
+        time.setStart(10L);
+        time.setStop(1L);
+    }
+
     @Test(expected = ClockOutBeforeClockInException.class)
     public void clockInAt_ClockInAfterClockOut_ThrowException()
         throws ClockOutBeforeClockInException {
