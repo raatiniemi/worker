7aafddf0bc84f7a0099a45e22ec84301fc560243
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-08-12 05:04:43 +0200

    Refactor DateIntervalFormat unit test
    Refactor the DateIntervalFormatTest to use the junit data provider
    to ease the testing of multiple values.
    
    Also, renamed the test methods to use the new naming convention.

--- app/src/test/java/me/raatiniemi/worker/util/DateIntervalFormatTest.java
+++ app/src/test/java/me/raatiniemi/worker/util/DateIntervalFormatTest.java
@@ -1,130 +1,58 @@
 package me.raatiniemi.worker.util;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
-
-import me.raatiniemi.worker.BuildConfig;
 
 import static junit.framework.Assert.assertEquals;
 
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@RunWith(DataProviderRunner.class)
 public class DateIntervalFormatTest {
-    @Test
-    public void testFormatHoursMinutes1m() {
-        String value = DateIntervalFormat.format(60000L);
+    @DataProvider
+    public static Object[][] format_HourMinutes() {
+        return new Object[][]{
+            { 60000L, "1m" },
+            { 600000L, "10m" },
+            { 1800000L, "30m" },
+            { 3600000L, "1h 0m" },
+            { 27000000L, "7h 30m" },
+            { 108000000L, "30h 0m" },
+            { 203100000L, "56h 25m" },
+            { 3580000L, "1h 0m" }
+        };
+    }
 
-        assertEquals("1m", value);
+    @DataProvider
+    public static Object[][] format_FractionHours() {
+        return new Object[][]{
+            { 900000L, "0.25" },
+            { 3600000L, "1.00" },
+            { 4500000L, "1.25" },
+            { 7175000L, "2.00" },
+            { 108000000L, "30.00" },
+            { 203100000L, "56.42" }
+        };
     }
 
     @Test
-    public void testFormatHoursMinutes10m() {
-        String value = DateIntervalFormat.format(600000L);
+    @UseDataProvider("format_HourMinutes")
+    public void format_HourMinutesAssertEqualValue_True(Long interval, String expected) {
+        String value = DateIntervalFormat.format(interval);
 
-        assertEquals("10m", value);
+        assertEquals(expected, value);
     }
 
     @Test
-    public void testFormatHoursMinutes30m() {
-        String value = DateIntervalFormat.format(1800000L);
-
-        assertEquals("30m", value);
-    }
-
-    @Test
-    public void testFormatHoursMinutes60m() {
-        String value = DateIntervalFormat.format(3600000L);
-
-        assertEquals("1h 0m", value);
-    }
-
-    @Test
-    public void testFormatHoursMinutes7h30m() {
-        String value = DateIntervalFormat.format(27000000L);
-
-        assertEquals("7h 30m", value);
-    }
-
-    @Test
-    public void testFormatHoursMinutes30h() {
-        String value = DateIntervalFormat.format(108000000L);
-
-        assertEquals("30h 0m", value);
-    }
-
-    @Test
-    public void testFormatHoursMinutes56h25m() {
-        String value = DateIntervalFormat.format(203100000L);
-
-        assertEquals("56h 25m", value);
-    }
-
-    @Test
-    public void testFormatHoursMinutes59m40s() {
-        String value = DateIntervalFormat.format(3580000L);
-
-        assertEquals("1h 0m", value);
-    }
-
-    @Test
-    public void testFormatFractionHours15m() {
+    @UseDataProvider("format_FractionHours")
+    public void format_FractionHoursAssertEqualValue_True(Long interval, String expected) {
         String value = DateIntervalFormat.format(
-            900000L,
+            interval,
             DateIntervalFormat.Type.FRACTION_HOURS
         );
 
-        assertEquals("0.25", value);
-    }
-
-    @Test
-    public void testFormatFractionHours1h() {
-        String value = DateIntervalFormat.format(
-            3600000L,
-            DateIntervalFormat.Type.FRACTION_HOURS
-        );
-
-        assertEquals("1.00", value);
-    }
-
-    @Test
-    public void testFormatFractionHours1h15m() {
-        String value = DateIntervalFormat.format(
-            4500000L,
-            DateIntervalFormat.Type.FRACTION_HOURS
-        );
-
-        assertEquals("1.25", value);
-    }
-
-    @Test
-    public void testFormatFractionHours1h59m35s() {
-        String value = DateIntervalFormat.format(
-            7175000L,
-            DateIntervalFormat.Type.FRACTION_HOURS
-        );
-
-        assertEquals("2.00", value);
-    }
-
-    @Test
-    public void testFormatFractionHours30h() {
-        String value = DateIntervalFormat.format(
-            108000000L,
-            DateIntervalFormat.Type.FRACTION_HOURS
-        );
-
-        assertEquals("30.00", value);
-    }
-
-    @Test
-    public void testFormatFractionHours56h25m() {
-        String value = DateIntervalFormat.format(
-            203100000L,
-            DateIntervalFormat.Type.FRACTION_HOURS
-        );
-
-        assertEquals("56.42", value);
+        assertEquals(expected, value);
     }
 }
