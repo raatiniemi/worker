7b5c9641b4f5992bccb20356b2a1704fcb6e603d
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-08-22 10:56:34 +0200

    Display group registered status based on children
    If the child items within the group have all been registered the group should
    be considered registered and display the same background color.

--- app/src/main/java/me/raatiniemi/worker/project/timesheet/TimesheetAdapter.java
+++ app/src/main/java/me/raatiniemi/worker/project/timesheet/TimesheetAdapter.java
@@ -65,10 +65,19 @@ public class TimesheetAdapter extends ExpandableListAdapter<
         vh.mTitle.setText(mDateFormat.format(date));
 
         long interval = 0;
+        boolean registered = true;
 
         for (Time time : get(group)) {
+            // If a single child is not registered, the group should not be
+            // considered registered.
+            if (0L == time.getRegistered()) {
+                registered = false;
+            }
+
             interval += time.getInterval();
         }
+
+        vh.itemView.setActivated(registered);
 
         String summarize = DateIntervalFormat.format(
             interval,
