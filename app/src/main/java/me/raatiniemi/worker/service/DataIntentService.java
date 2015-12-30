620136355c67023ca11c923e88a5531ec211595c
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-12-30 23:21:33 +0100

    refactor backup/restore using strategy pattern
    to improve testability the backup and restore operations should be
    moved to separate classes and invoked using a strategy pattern.

--- app/src/main/java/me/raatiniemi/worker/service/DataIntentService.java
+++ app/src/main/java/me/raatiniemi/worker/service/DataIntentService.java
@@ -17,25 +17,12 @@
 package me.raatiniemi.worker.service;
 
 import android.app.IntentService;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import de.greenrobot.event.EventBus;
-import me.raatiniemi.worker.R;
-import me.raatiniemi.worker.presentation.model.backup.Backup;
-import me.raatiniemi.worker.presentation.model.backup.BackupSuccessfulEvent;
-import me.raatiniemi.worker.presentation.view.activity.MainActivity;
-import me.raatiniemi.worker.util.ExternalStorage;
-import me.raatiniemi.worker.util.FileUtils;
-import me.raatiniemi.worker.util.Worker;
+import me.raatiniemi.worker.service.data.BackupStrategy;
+import me.raatiniemi.worker.service.data.RestoreStrategy;
 
 /**
  * Service for running data operations.
@@ -95,152 +82,19 @@ public class DataIntentService extends IntentService {
         switch (action) {
             case INTENT_ACTION_BACKUP:
                 sRunning = RUNNING.BACKUP;
-                backup();
+                BackupStrategy backupStrategy = new BackupStrategy(getApplicationContext(), mEventBus);
+                backupStrategy.execute();
                 break;
             case INTENT_ACTION_RESTORE:
                 sRunning = RUNNING.RESTORE;
-                restore();
+                RestoreStrategy restoreStrategy = new RestoreStrategy(getApplicationContext(), mEventBus);
+                restoreStrategy.execute();
                 break;
             default:
                 Log.w(TAG, "Received unknown action: " + action);
                 break;
         }
         sRunning = RUNNING.NONE;
-    }
-
-    /**
-     * Run the backup operation. Copies the SQLite database to the backup
-     * directory on the external storage.
-     */
-    private synchronized void backup() {
-        NotificationManager manager = null;
-        NotificationCompat.Builder notification = null;
-
-        try {
-            manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-            // Check that the external storage is writable.
-            if (!ExternalStorage.isWritable()) {
-                throw new IOException("External storage is not writable");
-            }
-
-            // Check that the backup directory is available.
-            File directory = ExternalStorage.getBackupDirectory();
-            if (null == directory) {
-                throw new FileNotFoundException("Directory for backup is not available");
-            }
-
-            // Retrieve the source and destination file locations.
-            File from = getDatabasePath(Worker.DATABASE_NAME);
-            File to = new File(directory, Worker.DATABASE_NAME);
-
-            // Perform the file copy.
-            FileUtils.copy(from, to);
-
-            // Send the "Backup complete" notification to the user.
-            notification = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.drawable.ic_archive_white_24dp)
-                    .setContentTitle(getString(R.string.notification_backup_title))
-                    .setContentText(getString(R.string.notification_backup_message));
-
-            // Assemble and post the successful backup event.
-            Backup backup = new Backup(directory);
-            mEventBus.post(new BackupSuccessfulEvent(backup));
-        } catch (IOException e) {
-            Log.w(TAG, "Unable to backup: " + e.getMessage());
-
-            // TODO: Display what was the cause of the backup failure.
-            // Send the "Backup failed" notification to the user.
-            notification = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.drawable.ic_error_outline_white_24dp)
-                    .setContentTitle(getString(R.string.error_notification_backup_title))
-                    .setContentText(getString(R.string.error_notification_backup_message));
-        } catch (ClassCastException e) {
-            Log.w(TAG, "Unable to cast the NotificationManager: " + e.getMessage());
-        } finally {
-            // Both the notification and notification manager must be
-            // available, otherwise we can't display the notification.
-            //
-            // The notification manager won't be available if a
-            // ClassCastException have been thrown.
-            if (null != manager && null != notification) {
-                manager.notify(
-                        Worker.NOTIFICATION_DATA_INTENT_SERVICE_ID,
-                        notification.build()
-                );
-            }
-        }
-    }
-
-    /**
-     * Run the restore operation. Copies the SQLite database from the latest
-     * backup directory on the external storage to the database location.
-     */
-    private synchronized void restore() {
-        NotificationManager manager = null;
-        NotificationCompat.Builder notification = null;
-
-        try {
-            manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-            // Check that the external storage is readable.
-            if (!ExternalStorage.isReadable()) {
-                throw new IOException("External storage is not readable");
-            }
-
-            // Check that we have backup to restore from.
-            File directory = ExternalStorage.getLatestBackupDirectory();
-            if (null == directory) {
-                throw new FileNotFoundException("Unable to find backup from which to restore");
-            }
-
-            // Retrieve the source and destination file locations.
-            File from = new File(directory, Worker.DATABASE_NAME);
-            File to = getDatabasePath(Worker.DATABASE_NAME);
-
-            // Perform the file copy.
-            FileUtils.copy(from, to);
-
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.setAction(Worker.INTENT_ACTION_RESTART);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-            PendingIntent pendingIntent = PendingIntent.getActivity(
-                    this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT
-            );
-
-            // Send the "Restore complete" notification to the user.
-            notification = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.drawable.ic_restore_white_24dp)
-                    .setContentTitle(getString(R.string.notification_restore_title))
-                    .setContentText(getString(R.string.notification_restore_message))
-                    .setContentIntent(pendingIntent)
-                    .setAutoCancel(true);
-        } catch (IOException e) {
-            Log.w(TAG, "Unable to restore backup: " + e.getMessage());
-
-            // TODO: Display what was the cause of the restore failure.
-            // Send the "Restore failed" notification to the user.
-            notification = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.drawable.ic_error_outline_white_24dp)
-                    .setContentTitle(getString(R.string.error_notification_restore_title))
-                    .setContentText(getString(R.string.error_notification_restore_message));
-        } catch (ClassCastException e) {
-            Log.w(TAG, "Unable to cast the NotificationManager: " + e.getMessage());
-        } finally {
-            // Both the notification and notification manager must be
-            // available, otherwise we can't display the notification.
-            //
-            // The notification manager won't be available if a
-            // ClassCastException have been thrown.
-            if (null != manager && null != notification) {
-                manager.notify(
-                        Worker.NOTIFICATION_DATA_INTENT_SERVICE_ID,
-                        notification.build()
-                );
-            }
-        }
     }
 
     /**
