b8e2f9b6a1a4b0f380d41e983beff447ede74fb0
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-12-31 09:24:50 +0100

    add base representation for row from database

---
+++ app/src/test/java/me/raatiniemi/worker/data/entity/BaseEntityTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015 Worker Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.data.entity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import me.raatiniemi.worker.BuildConfig;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class BaseEntityTest {
+    @Test
+    public void getId_valueFromConstructor() {
+        BaseEntity entity = new BaseEntity(1);
+
+        assertEquals(1, entity.getId());
+    }
+}
