483dd73e6212f5bef551e00ef86885c41c657ab9
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-01-11 19:27:46 +0100

    add interface for backup strategies

---
+++ app/src/main/java/me/raatiniemi/worker/service/data/BackupStrategy.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2016 Worker Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.service.data;
+
+/**
+ * Interface for different backup strategies.
+ * <p>
+ * TODO: Move BackupStrategy to the correct package.
+ */
+public interface BackupStrategy {
+    /**
+     * Run the backup process.
+     */
+    void execute();
+}
