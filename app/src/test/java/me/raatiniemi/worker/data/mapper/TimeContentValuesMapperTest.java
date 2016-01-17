e89eb2d0adb7060335286725696ff8ea7a1ce612
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-01-17 09:24:46 +0100

    add ContentValues mapper for Time entities

---
+++ app/src/test/java/me/raatiniemi/worker/data/mapper/TimeContentValuesMapperTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2016 Worker Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.data.mapper;
+
+import android.content.ContentValues;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import me.raatiniemi.worker.BuildConfig;
+import me.raatiniemi.worker.data.WorkerContract.TimeColumns;
+import me.raatiniemi.worker.domain.exception.ClockOutBeforeClockInException;
+import me.raatiniemi.worker.domain.model.Time;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class TimeContentValuesMapperTest {
+    private static ContentValues createContentValues(
+            final long projectId,
+            final long start,
+            final long stop,
+            final long registered
+    ) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TimeColumns.PROJECT_ID, projectId);
+        contentValues.put(TimeColumns.START, start);
+        contentValues.put(TimeColumns.STOP, stop);
+        contentValues.put(TimeColumns.REGISTERED, registered);
+
+        return contentValues;
+    }
+
+    private static Time createTime(
+            final long projectId,
+            final long start,
+            final long stop,
+            final boolean registered
+    ) throws ClockOutBeforeClockInException {
+        Time time = new Time(null, projectId, start, stop);
+        time.setRegistered(registered);
+
+        return time;
+    }
+
+    @Test
+    public void transform()
+            throws ClockOutBeforeClockInException {
+        TimeContentValuesMapper entityMapper = new TimeContentValuesMapper();
+
+        ContentValues expected = createContentValues(1L, 1L, 1L, 0L);
+        Time time = createTime(1L, 1L, 1L, false);
+        ContentValues contentValues = entityMapper.transform(time);
+
+        // the id column should not be mapped since that would introduce the
+        // possibility of the id being modified.
+        assertNull(contentValues.get(TimeColumns._ID));
+        assertEquals(expected.get(TimeColumns.PROJECT_ID), contentValues.get(TimeColumns.PROJECT_ID));
+        assertEquals(expected.get(TimeColumns.START), contentValues.get(TimeColumns.START));
+        assertEquals(expected.get(TimeColumns.STOP), contentValues.get(TimeColumns.STOP));
+        assertEquals(expected.get(TimeColumns.REGISTERED), contentValues.get(TimeColumns.REGISTERED));
+
+        expected = createContentValues(1L, 1L, 1L, 1L);
+        time = createTime(1L, 1L, 1L, true);
+        contentValues = entityMapper.transform(time);
+
+        // the id column should not be mapped since that would introduce the
+        // possibility of the id being modified.
+        assertNull(contentValues.get(TimeColumns._ID));
+        assertEquals(expected.get(TimeColumns.PROJECT_ID), contentValues.get(TimeColumns.PROJECT_ID));
+        assertEquals(expected.get(TimeColumns.START), contentValues.get(TimeColumns.START));
+        assertEquals(expected.get(TimeColumns.STOP), contentValues.get(TimeColumns.STOP));
+        assertEquals(expected.get(TimeColumns.REGISTERED), contentValues.get(TimeColumns.REGISTERED));
+    }
+}
