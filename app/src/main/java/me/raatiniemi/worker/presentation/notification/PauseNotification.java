8419146261603969bfaf7ea19f08dedc7e9a14e0
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-03-31 18:56:44 +0200

    add PauseNotification

---
+++ app/src/main/java/me/raatiniemi/worker/presentation/notification/PauseNotification.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2016 Worker Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.presentation.notification;
+
+import android.app.Notification;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+
+import me.raatiniemi.worker.R;
+
+/**
+ * Notification for pausing or clocking out an active project.
+ */
+public class PauseNotification {
+    private static final int sSmallIcon = R.drawable.ic_timer_black_24dp;
+
+    private static final int sPauseIcon = 0;
+
+    private static final int sClockOutIcon = 0;
+
+    public static Notification build(Context context) {
+        return new NotificationCompat.Builder(context)
+                .setContentTitle("Project name")
+                .setSmallIcon(sSmallIcon)
+                .addAction(buildPauseAction(context))
+                .addAction(buildClockOutAction(context))
+                .build();
+    }
+
+    private static NotificationCompat.Action buildPauseAction(Context context) {
+        return new NotificationCompat.Action(
+                sPauseIcon,
+                context.getString(R.string.notification_pause_action_pause),
+                null
+        );
+    }
+
+    private static NotificationCompat.Action buildClockOutAction(Context context) {
+        return new NotificationCompat.Action(
+                sClockOutIcon,
+                context.getString(R.string.notification_pause_action_clock_out),
+                null
+        );
+    }
+}
