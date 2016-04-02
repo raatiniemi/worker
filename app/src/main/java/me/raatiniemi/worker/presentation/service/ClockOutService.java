21f4d90f2409fe970883392632d335ea2680e14c
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-04-02 20:30:46 +0200

    clock out project via ClockOutService

--- app/src/main/java/me/raatiniemi/worker/presentation/service/ClockOutService.java
+++ app/src/main/java/me/raatiniemi/worker/presentation/service/ClockOutService.java
@@ -18,6 +18,16 @@ package me.raatiniemi.worker.presentation.service;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
+
+import java.util.Date;
+
+import me.raatiniemi.worker.data.WorkerContract;
+import me.raatiniemi.worker.data.mapper.TimeContentValuesMapper;
+import me.raatiniemi.worker.data.mapper.TimeCursorMapper;
+import me.raatiniemi.worker.data.repository.TimeResolverRepository;
+import me.raatiniemi.worker.domain.interactor.ClockOut;
+import me.raatiniemi.worker.domain.repository.TimeRepository;
 
 public class ClockOutService extends IntentService {
     private static final String TAG = "ClockOutService";
@@ -28,5 +38,29 @@ public class ClockOutService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        try {
+            ClockOut clockOut = new ClockOut(getTimeRepository());
+            clockOut.execute(getProjectId(intent), new Date());
+        } catch (Exception e) {
+            Log.w(TAG, "Unable to clock out project: " + e.getMessage());
+        }
+    }
+
+    private TimeRepository getTimeRepository() {
+        return new TimeResolverRepository(
+                getContentResolver(),
+                new TimeCursorMapper(),
+                new TimeContentValuesMapper()
+        );
+    }
+
+    private long getProjectId(Intent intent) {
+        String itemId = WorkerContract.ProjectContract.getItemId(intent.getData());
+        long projectId = Long.valueOf(itemId);
+        if (0 == projectId) {
+            throw new IllegalArgumentException("Unable to extract project id from URI");
+        }
+
+        return projectId;
     }
 }
