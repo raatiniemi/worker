450366e5d1927eb7d78fa9eaab115ed52d756800
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-04-11 18:18:07 +0200

    add unit test for ClockIn

---
+++ app/src/test/java/me/raatiniemi/worker/domain/interactor/ClockInTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2016 Worker Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.domain.interactor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Date;
+
+import me.raatiniemi.worker.domain.exception.ClockActivityException;
+import me.raatiniemi.worker.domain.exception.DomainException;
+import me.raatiniemi.worker.domain.model.Time;
+import me.raatiniemi.worker.domain.repository.TimeRepository;
+
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(JUnit4.class)
+public class ClockInTest {
+    private TimeRepository mTimeRepository;
+
+    @Before
+    public void setUp() {
+        mTimeRepository = mock(TimeRepository.class);
+    }
+
+    @Test(expected = ClockActivityException.class)
+    public void execute_withActiveTime() throws DomainException {
+        Time time = new Time.Builder(1L)
+                .build();
+
+        when(mTimeRepository.getActiveTimeForProject(1L))
+                .thenReturn(time);
+
+        ClockIn clockIn = new ClockIn(mTimeRepository);
+        clockIn.execute(1L, new Date());
+    }
+
+    @Test
+    public void execute() throws DomainException {
+        when(mTimeRepository.getActiveTimeForProject(1L))
+                .thenReturn(null);
+
+        ClockIn clockIn = new ClockIn(mTimeRepository);
+        clockIn.execute(1L, new Date());
+
+        verify(mTimeRepository).add(isA(Time.class));
+    }
+}
