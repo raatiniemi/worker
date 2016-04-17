ad1ec09aa172307a20fe6fe92dfc6969333b74e3
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-04-17 20:21:17 +0200

    add unit test for GetProject

---
+++ app/src/test/java/me/raatiniemi/worker/domain/interactor/GetProjectTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2016 Worker Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.domain.interactor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import me.raatiniemi.worker.domain.exception.DomainException;
+import me.raatiniemi.worker.domain.repository.ProjectRepository;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(JUnit4.class)
+public class GetProjectTest {
+    private ProjectRepository mProjectRepository;
+
+    @Before
+    public void setUp() {
+        mProjectRepository = mock(ProjectRepository.class);
+    }
+
+    @Test
+    public void execute() throws DomainException {
+        GetProject getProject = new GetProject(mProjectRepository);
+        getProject.execute(1L);
+
+        verify(mProjectRepository).get(1L);
+    }
+}
