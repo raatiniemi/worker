505e3b5e14dedb3b4ac1036adfc1e4f6c35fd986
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-04-17 20:50:01 +0200

    add unit test for RemoveProject

---
+++ app/src/test/java/me/raatiniemi/worker/domain/interactor/RemoveProjectTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 Worker Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.domain.interactor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import me.raatiniemi.worker.domain.exception.DomainException;
+import me.raatiniemi.worker.domain.model.Project;
+import me.raatiniemi.worker.domain.repository.ProjectRepository;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(JUnit4.class)
+public class RemoveProjectTest {
+    private ProjectRepository mProjectRepository;
+
+    @Before
+    public void setUp() {
+        mProjectRepository = mock(ProjectRepository.class);
+    }
+
+    @Test
+    public void execute() throws DomainException {
+        Project project = new Project.Builder("Project name")
+                .id(1L)
+                .build();
+
+        RemoveProject removeProject = new RemoveProject(mProjectRepository);
+        removeProject.execute(project);
+
+        verify(mProjectRepository).remove(eq(1L));
+    }
+}
