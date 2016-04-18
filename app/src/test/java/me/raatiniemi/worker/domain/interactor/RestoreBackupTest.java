2fc66ec54692f3e8d1bc3ff2fa25ca51dc51e1cf
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-04-18 17:44:21 +0200

    add unit test for RestoreBackup

---
+++ app/src/test/java/me/raatiniemi/worker/domain/interactor/RestoreBackupTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2016 Worker Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.domain.interactor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(JUnit4.class)
+public class RestoreBackupTest {
+    private RestoreStrategy mRestoreStrategy;
+
+    @Before
+    public void setUp() {
+        mRestoreStrategy = mock(RestoreStrategy.class);
+    }
+
+    @Test
+    public void execute() {
+        RestoreBackup restoreBackup = new RestoreBackup(mRestoreStrategy);
+        restoreBackup.execute();
+
+        verify(mRestoreStrategy).execute();
+    }
+}
