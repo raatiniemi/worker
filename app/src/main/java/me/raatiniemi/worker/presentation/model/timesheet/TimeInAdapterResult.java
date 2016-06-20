a1b79bb3dacb9fa433c09b5002e1e80e15d6f43e
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-06-20 20:47:49 +0200

    add equals and hash code for TimeInAdapterResult

--- app/src/main/java/me/raatiniemi/worker/presentation/model/timesheet/TimeInAdapterResult.java
+++ app/src/main/java/me/raatiniemi/worker/presentation/model/timesheet/TimeInAdapterResult.java
@@ -16,6 +16,8 @@
 
 package me.raatiniemi.worker.presentation.model.timesheet;
 
+import java.util.Objects;
+
 import me.raatiniemi.worker.domain.model.Time;
 
 public final class TimeInAdapterResult {
@@ -41,5 +43,31 @@ public final class TimeInAdapterResult {
 
     public Time getTime() {
         return mTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof TimeInAdapterResult)) {
+            return false;
+        }
+
+        TimeInAdapterResult result = (TimeInAdapterResult) o;
+        return mGroup == result.mGroup
+                && mChild == result.mChild
+                && Objects.equals(mTime, result.mTime);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + mGroup;
+        result = 31 * result + mChild;
+        result = 31 * result + Objects.hashCode(mTime);
+
+        return result;
     }
 }
