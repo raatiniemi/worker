3eae29f5cd2ac2826eb7281844746f5a01e767ea
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-08-14 14:18:11 +0200

    hide MESSAGE_PROJECT_ID from outside the package
    MESSAGE_PROJECT_ID is only referenced from within the package and can
    be made package private.

--- app/src/main/java/me/raatiniemi/worker/presentation/project/view/ProjectActivity.java
+++ app/src/main/java/me/raatiniemi/worker/presentation/project/view/ProjectActivity.java
@@ -27,7 +27,7 @@ import me.raatiniemi.worker.presentation.util.Settings;
 import me.raatiniemi.worker.presentation.view.activity.BaseActivity;
 
 public class ProjectActivity extends BaseActivity {
-    public static final String MESSAGE_PROJECT_ID = "me.raatiniemi.activity.project.id";
+    static final String MESSAGE_PROJECT_ID = "me.raatiniemi.activity.project.id";
 
     /**
      * Tag for the timesheet fragment.
