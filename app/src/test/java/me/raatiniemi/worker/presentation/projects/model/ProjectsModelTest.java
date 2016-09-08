ef4e54e0189ac03caec1bd1b368b83ac79e69252
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-09-08 18:47:51 +0200

    separate tests for ProjectsModel::setVisibilityForClockedInSinceView

--- app/src/test/java/me/raatiniemi/worker/presentation/projects/model/ProjectsModelTest.java
+++ app/src/test/java/me/raatiniemi/worker/presentation/projects/model/ProjectsModelTest.java
@@ -16,51 +16,19 @@
 
 package me.raatiniemi.worker.presentation.projects.model;
 
-import android.view.View;
-import android.widget.TextView;
-
-import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import me.raatiniemi.worker.domain.exception.ClockOutBeforeClockInException;
 import me.raatiniemi.worker.domain.exception.InvalidProjectNameException;
 import me.raatiniemi.worker.domain.model.Project;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(DataProviderRunner.class)
 public class ProjectsModelTest {
-    @DataProvider
-    public static Object[][] setVisibilityForClockedInSinceView_dataProvider()
-            throws ClockOutBeforeClockInException {
-        return new Object[][]{
-                {
-                        mockProjectWithActiveIndicator(Boolean.FALSE),
-                        View.GONE
-                },
-                {
-                        mockProjectWithActiveIndicator(Boolean.TRUE),
-                        View.VISIBLE
-                }
-        };
-    }
-
-    private static Project mockProjectWithActiveIndicator(boolean isProjectActive) {
-        Project project = mock(Project.class);
-        when(project.isActive()).thenReturn(isProjectActive);
-
-        return project;
-    }
-
     private Project.Builder createProjectBuilder(String projectName) {
         return new Project.Builder(projectName);
     }
@@ -84,19 +52,5 @@ public class ProjectsModelTest {
         ProjectsModel model = new ProjectsModel(project);
 
         assertEquals("Project name", model.getTitle());
-    }
-
-    @Test
-    @UseDataProvider("setVisibilityForClockedInSinceView_dataProvider")
-    public void setVisibilityForClockedInSinceView(
-            Project project,
-            int expectedViewVisibility
-    ) throws InvalidProjectNameException, ClockOutBeforeClockInException {
-        ProjectsModel model = new ProjectsModel(project);
-        TextView textView = mock(TextView.class);
-
-        model.setVisibilityForClockedInSinceView(textView);
-
-        verify(textView, times(1)).setVisibility(expectedViewVisibility);
     }
 }
