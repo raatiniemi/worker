ba996eb8e213fb06d53b54403ecf1879bba7e27a
Tobias Raatiniemi <raatiniemi@gmail.com>
2018-12-20 06:06:12 +0100

    convert ProjectsAdapter to kotlin

---
+++ app/src/main/java/me/raatiniemi/worker/features/projects/view/ProjectsAdapter.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2018 Tobias Raatiniemi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.features.projects.view
+
+import android.content.res.Resources
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import me.raatiniemi.worker.R
+import me.raatiniemi.worker.features.projects.model.ProjectsItem
+import me.raatiniemi.worker.features.projects.model.ProjectsItemAdapterResult
+import me.raatiniemi.worker.features.shared.view.adapter.SimpleListAdapter
+import me.raatiniemi.worker.util.HintedImageButtonListener
+
+internal class ProjectsAdapter(
+        private val resources: Resources,
+        private val onProjectActionListener: OnProjectActionListener,
+        private val hintedImageButtonListener: HintedImageButtonListener
+) : SimpleListAdapter<ProjectsItem, ProjectsItemViewHolder>() {
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.fragment_projects_item
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ProjectsItemViewHolder {
+        val inflater = LayoutInflater.from(viewGroup.context)
+        val view = inflater.inflate(viewType, viewGroup, false)
+
+        return ProjectsItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(vh: ProjectsItemViewHolder, index: Int) {
+        val item = get(index)
+        val result = ProjectsItemAdapterResult(index, item)
+
+        vh.apply {
+            name.text = item.title
+            time.text = item.timeSummary
+
+            clockedInSince.text = item.getClockedInSince(resources)
+            item.setVisibilityForClockedInSinceView(clockedInSince)
+
+            itemView.setOnClickListener(onClickListener)
+
+            with(clockActivityToggle) {
+                contentDescription = item.getHelpTextForClockActivityToggle(resources)
+                setOnClickListener { onProjectActionListener.onClockActivityToggle(result) }
+                setOnLongClickListener(hintedImageButtonListener)
+                isActivated = item.isActive
+            }
+
+            with(clockActivityAt) {
+                contentDescription = item.getHelpTextForClockActivityAt(resources)
+                setOnClickListener { onProjectActionListener.onClockActivityAt(result) }
+                setOnLongClickListener(hintedImageButtonListener)
+            }
+
+            with(delete) {
+                contentDescription = item.getHelpTextForDelete(resources)
+                setOnClickListener { onProjectActionListener.onDelete(result) }
+                setOnLongClickListener(hintedImageButtonListener)
+            }
+        }
+    }
+}
