769e6a8cbfbba859daa4480f4e976feba7a84847
Tobias Raatiniemi <raatiniemi@gmail.com>
2019-02-13 06:20:34 +0100

    convert TimeSummaryStartingPointChangeEvent to kotlin

--- app/src/main/java/me/raatiniemi/worker/features/settings/project/model/TimeSummaryStartingPointChangeEvent.kt
+++ app/src/main/java/me/raatiniemi/worker/features/settings/project/model/TimeSummaryStartingPointChangeEvent.kt
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package me.raatiniemi.worker.features.settings.project.model;
+package me.raatiniemi.worker.features.settings.project.model
 
-public class TimeSummaryStartingPointChangeEvent {
-}
+class TimeSummaryStartingPointChangeEvent
