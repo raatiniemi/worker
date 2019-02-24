1f30bc089be302af9ce55a5a877b32adee1cce60
Tobias Raatiniemi <raatiniemi@gmail.com>
2019-02-24 10:39:15 +0100

    move key value store interface to core module

--- core/src/main/java/me/raatiniemi/worker/util/KeyValueStore.kt
+++ core/src/main/java/me/raatiniemi/worker/util/KeyValueStore.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Tobias Raatiniemi
+ * Copyright (C) 2019 Tobias Raatiniemi
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
