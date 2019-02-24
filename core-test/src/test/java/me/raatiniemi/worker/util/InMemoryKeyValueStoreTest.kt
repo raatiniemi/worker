08fa6d33e59ca9d5134748ae03b9240a3dcb8e65
Tobias Raatiniemi <raatiniemi@gmail.com>
2019-02-24 10:51:40 +0100

    add unit tests for InMemoryKeyValueStore

---
+++ core-test/src/test/java/me/raatiniemi/worker/util/InMemoryKeyValueStoreTest.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2019 Tobias Raatiniemi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.util
+
+import me.raatiniemi.worker.domain.model.TimeIntervalStartingPoint
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class InMemoryKeyValueStoreTest {
+    private lateinit var keyValueStore: KeyValueStore
+
+    @Before
+    fun setUp() {
+        keyValueStore = InMemoryKeyValueStore()
+    }
+
+    @Test
+    fun `bool without value for key`() {
+        val expected = false
+
+        val actual = keyValueStore.bool(AppKeys.HIDE_REGISTERED_TIME.rawValue, expected)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `bool with invalid type for key`() {
+        val expected = false
+        keyValueStore.set(AppKeys.HIDE_REGISTERED_TIME.rawValue, 1)
+
+        val actual = keyValueStore.bool(AppKeys.HIDE_REGISTERED_TIME.rawValue, expected)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `bool with value for key`() {
+        val expected = true
+        keyValueStore.set(AppKeys.HIDE_REGISTERED_TIME.rawValue, true)
+
+        val actual = keyValueStore.bool(AppKeys.HIDE_REGISTERED_TIME.rawValue, false)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `int without value for key`() {
+        val expected = TimeIntervalStartingPoint.WEEK.rawValue
+
+        val actual = keyValueStore.int(AppKeys.TIME_SUMMARY.rawValue, expected)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `int with invalid type for key`() {
+        val expected = TimeIntervalStartingPoint.WEEK.rawValue
+        keyValueStore.set(AppKeys.TIME_SUMMARY.rawValue, false)
+
+        val actual = keyValueStore.int(AppKeys.TIME_SUMMARY.rawValue, expected)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `int with value for key`() {
+        val expected = TimeIntervalStartingPoint.WEEK.rawValue
+        keyValueStore.set(AppKeys.TIME_SUMMARY.rawValue, expected)
+
+        val actual = keyValueStore.int(
+                AppKeys.TIME_SUMMARY.rawValue,
+                TimeIntervalStartingPoint.MONTH.rawValue
+        )
+
+        assertEquals(expected, actual)
+    }
+}
