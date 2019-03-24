016a3065a9c0b7b833d1566010f16cd322c642d0
Tobias Raatiniemi <raatiniemi@gmail.com>
2019-03-24 17:08:23 +0100

    convert resume service to kotlin

---
+++ app/src/main/java/me/raatiniemi/worker/data/service/ongoing/ResumeService.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2018 Tobias Raatiniemi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.data.service.ongoing
+
+import android.content.Intent
+import me.raatiniemi.worker.R
+import me.raatiniemi.worker.domain.exception.ActiveProjectException
+import me.raatiniemi.worker.domain.interactor.ClockIn
+import me.raatiniemi.worker.domain.interactor.GetProject
+import me.raatiniemi.worker.domain.model.Project
+import me.raatiniemi.worker.features.shared.view.notification.ErrorNotification
+import me.raatiniemi.worker.features.shared.view.notification.PauseNotification
+import timber.log.Timber
+import java.util.*
+
+internal class ResumeService : OngoingService("ResumeService") {
+    override fun onHandleIntent(intent: Intent?) {
+        val projectId = getProjectId(intent)
+
+        try {
+            clockInProjectNow(projectId)
+
+            updateUserInterface(projectId)
+
+            if (isOngoingNotificationEnabled) {
+                val getProject = buildGetProjectUseCase()
+                val project = getProject.execute(projectId)
+
+                sendPauseNotification(project)
+                return
+            }
+            dismissResumeNotification(projectId)
+        } catch (e: Exception) {
+            Timber.w(e, "Unable to resume project")
+            sendErrorNotification(projectId)
+        }
+    }
+
+    private fun clockInProjectNow(projectId: Long) {
+        try {
+            val clockIn = buildClockInUseCase()
+            clockIn(projectId, Date())
+        } catch (e: ActiveProjectException) {
+            Timber.e(e, "Resume service called with active project")
+        }
+    }
+
+    private fun buildClockInUseCase(): ClockIn {
+        return ClockIn(timeIntervalRepository)
+    }
+
+    private fun buildGetProjectUseCase(): GetProject {
+        return GetProject(projectRepository)
+    }
+
+    private fun sendPauseNotification(project: Project) {
+        sendNotification(
+                project.id,
+                PauseNotification.build(this, project, isOngoingNotificationChronometerEnabled)
+        )
+    }
+
+    private fun dismissResumeNotification(projectId: Long) {
+        dismissNotification(projectId)
+    }
+
+    private fun sendErrorNotification(projectId: Long) {
+        sendNotification(
+                projectId,
+                ErrorNotification.buildOngoing(
+                        this,
+                        getString(R.string.error_notification_resume_title),
+                        getString(R.string.error_notification_resume_message)
+                )
+        )
+    }
+}
