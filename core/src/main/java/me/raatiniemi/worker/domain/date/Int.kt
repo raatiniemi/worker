cefca9e52fefd9a900bca4401da176391b9491d8
Tobias Raatiniemi <raatiniemi@gmail.com>
2019-04-05 06:38:43 +0200

    add time interval properties for Int/Long

---
+++ core/src/main/java/me/raatiniemi/worker/domain/date/Int.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2019 Tobias Raatiniemi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.raatiniemi.worker.domain.date
+
+val Int.milliseconds: Long
+    get() = toLong().milliseconds
+
+val Int.seconds: Long
+    get() = toLong().seconds
+
+val Int.minutes: Long
+    get() = toLong().minutes
+
+val Int.hours: Long
+    get() = toLong().hours
