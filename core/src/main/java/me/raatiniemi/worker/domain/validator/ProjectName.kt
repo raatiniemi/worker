a34c2a18ac2f3dba82ee400b7d272d2c5acd50ac
Tobias Raatiniemi <raatiniemi@gmail.com>
2019-06-02 13:30:41 +0200

    convert ProjectName validator to kotlin

--- core/src/main/java/me/raatiniemi/worker/domain/validator/ProjectName.kt
+++ core/src/main/java/me/raatiniemi/worker/domain/validator/ProjectName.kt
@@ -14,15 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package me.raatiniemi.worker.domain.validator;
+package me.raatiniemi.worker.domain.validator
 
-import static me.raatiniemi.worker.util.NullUtil.nonNull;
-
-public final class ProjectName {
-    private ProjectName() {
-    }
-
-    public static boolean isValid(String name) {
-        return nonNull(name) && !name.isEmpty();
-    }
+object ProjectName {
+    fun isValid(name: String?): Boolean = !name.isNullOrEmpty()
 }
