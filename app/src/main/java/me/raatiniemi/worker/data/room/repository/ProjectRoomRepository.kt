1e7d316fb2328811cc427188d0fcf35e925f7432
Tobias Raatiniemi <raatiniemi@gmail.com>
2020-01-15 07:05:08 +0100

    update year in copyright header to 2020

--- app/src/main/java/me/raatiniemi/worker/data/room/repository/ProjectRoomRepository.kt
+++ app/src/main/java/me/raatiniemi/worker/data/room/repository/ProjectRoomRepository.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Tobias Raatiniemi
+ * Copyright (C) 2020 Tobias Raatiniemi
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
