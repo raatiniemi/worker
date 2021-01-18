34103c3fb688f78c228549387aa44ac77d368d8b
Tobias Raatiniemi <raatiniemi@gmail.com>
2021-01-18 20:09:10 +0100

    remove unused import statement

--- app/src/test/java/me/raatiniemi/worker/feature/projects/all/model/ProjectsItemIsActiveTest.kt
+++ app/src/test/java/me/raatiniemi/worker/feature/projects/all/model/ProjectsItemIsActiveTest.kt
@@ -26,7 +26,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
-import java.util.*
 
 @RunWith(Parameterized::class)
 class ProjectsItemIsActiveTest(
